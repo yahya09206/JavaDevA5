deae567d08a958912a12aa31b7959c6fbb1f1ab5
Yahya <yahyahussein91@gmail.com>
2023-07-24 22:05:57 -0700

    warming up

--- JavaProgammingRefresh/src/practice/PrintAndMain.java
+++ JavaProgammingRefresh/src/practice/PrintAndMain.java
@@ -5,5 +5,6 @@ public class PrintAndMain {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        System.out.println("What's up doc?");
     }
 }
---
+++ JavaProgammingRefresh/src/practice/WoodenSpoon.java
@@ -0,0 +1,8 @@
+package practice;
+
+public class WoodenSpoon {
+    public static void main(String[] args) {
+
+        System.out.println("More printing random stuff");
+    }
+}
