ee3be75e572af979d5bbb892857d329a874f53e8
Yahya <yahyahussein91@gmail.com>
2023-08-16 20:28:24 -0700

    arrylist review

---
+++ JavaProgammingRefresh/src/practice/CollectionsReview/ArrayListReview.java
@@ -0,0 +1,20 @@
+package practice.CollectionsReview;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListReview {
+    public static void main(String[] args) {
+
+        // Create ArrayList and a Class
+        List<Student> students = new ArrayList<>();
+
+        // Add elements to ArrayList
+        students.add(new Student(1, "Jose"));
+        students.add(new Student(2, "Sayed"));
+        students.add(new Student(3, "Nick"));
+        students.add(new Student(4, "Ronnie"));
+
+
+    }
+}
---
+++ JavaProgammingRefresh/src/practice/CollectionsReview/Student.java
@@ -0,0 +1,12 @@
+package practice.CollectionsReview;
+
+public class Student {
+
+    int id;
+    String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
