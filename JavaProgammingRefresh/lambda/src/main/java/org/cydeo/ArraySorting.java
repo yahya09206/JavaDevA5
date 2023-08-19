fd734b5719484773764f007855884537a66c376a
Yahya <yahyahussein91@gmail.com>
2023-08-19 09:26:23 -0700

    mock sorting array

---
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/ArraySorting.java
@@ -0,0 +1,14 @@
+package org.cydeo;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+
+        ArraySorting as = new ArraySorting();
+        as.sort();
+    }
+
+    public static void sort(){
+        System.out.println("Quick sorting Array");
+    }
+
+}
--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/Main.java
+++
@@ -1,7 +0,0 @@
-package org.cydeo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
