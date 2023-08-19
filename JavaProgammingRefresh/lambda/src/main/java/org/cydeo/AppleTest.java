6309b7dfea268f9840eec0fa779e2c664c58283b
Yahya <yahyahussein91@gmail.com>
2023-08-19 09:54:36 -0700

    method for filtering thru apples

--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/AppleTest.java
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/AppleTest.java
@@ -12,7 +12,19 @@ public class AppleTest {
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
 
+        List<Apple> heavyApple = filterApples(inventory, new AppleHeavyPredicate());
 
+    }
 
+    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate){
+
+        List<Apple> result = new ArrayList<>();
+        for (Apple apple : inventory){
+            if (applePredicate.test(apple)){
+                result.add(apple);
+            }
+        }
+
+        return result;
     }
 }
