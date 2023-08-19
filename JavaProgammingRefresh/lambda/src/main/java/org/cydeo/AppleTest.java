daa850f85e6455bc449108e80fa5d2e9ed3c7f3b
Yahya <yahyahussein91@gmail.com>
2023-08-19 10:04:18 -0700

    not working will be back later

--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/AppleTest.java
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/AppleTest.java
@@ -6,13 +6,17 @@ import java.util.List;
 public class AppleTest {
     public static void main(String[] args) {
 
-        List<Apple> inventory = new ArrayList<>();
+        List<Apple> inventory = new ArrayList<Apple>();
         inventory.add(new Apple(603, Color.GREEN));
         inventory.add(new Apple(100, Color.RED));
         inventory.add(new Apple(200, Color.GREEN));
         inventory.add(new Apple(50, Color.RED));
 
         List<Apple> heavyApple = filterApples(inventory, new AppleHeavyPredicate());
+        System.out.println(heavyApple);
+
+        List<Apple> greenApple = filterApples(inventory, new AppleHeavyPredicate());
+        System.out.println(greenApple);
 
     }
 
--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/Color.java
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/Color.java
@@ -3,5 +3,5 @@ package org.cydeo;
 public enum Color {
 
     RED,
-    GREEN
+    GREEN;
 }
