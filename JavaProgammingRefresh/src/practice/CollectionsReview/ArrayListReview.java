ecf26280997d6c2e42a5c99130c19a4a01dcfbc1
Yahya <yahyahussein91@gmail.com>
2023-08-18 21:42:11 -0700

    printing with lambda

--- JavaProgammingRefresh/src/practice/CollectionsReview/ArrayListReview.java
+++ JavaProgammingRefresh/src/practice/CollectionsReview/ArrayListReview.java
@@ -3,6 +3,7 @@ package practice.CollectionsReview;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class ArrayListReview {
     public static void main(String[] args) {
@@ -21,24 +22,33 @@ public class ArrayListReview {
             System.out.println(students.get(i));
         }
 
-        System.out.println("----------------------");
+        System.out.println("Reverse----------------------");
 
         // backward
         for (int i = students.size() - 1; i >= 0; i--){
             System.out.println(students.get(i));
         }
 
+        System.out.println("Iterator----------------------");
         // 2. Iterator
         Iterator iter = students.listIterator();
         while (iter.hasNext()){
             System.out.println(iter.next());
         }
 
-        System.out.println("----------------------");
+        System.out.println("Iterator reverse----------------------");
+        // 2. Iterator
+        while (((ListIterator<?>) iter).hasPrevious()){
+            System.out.println(((ListIterator<?>) iter).previous());
+        }
+
+        System.out.println("forEach()----------------------");
         // for each
         for (Student student : students) {
             System.out.println(student);
         }
 
+        System.out.println("Lambda-------------------");
+        students.forEach(student -> System.out.println(student));
     }
 }
