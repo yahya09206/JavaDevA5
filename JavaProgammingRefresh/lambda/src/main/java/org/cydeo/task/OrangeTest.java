dbece6318c53ff0849de29c987cf50de0caf79bc
Yahya <yahyahussein91@gmail.com>
2023-08-21 22:37:21 -0700

    constructing what to print

--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/task/OrangeTest.java
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/task/OrangeTest.java
@@ -9,5 +9,15 @@ public class OrangeTest {
         List<Orange> inventory = new ArrayList<>();
         inventory.add(new Orange(300, Color.GREEN));
         inventory.add(Orange.builder().weight(200).color(Color.GREEN).build());
+        inventory.add(Orange.builder().weight(100).color(Color.RED).build());
+        inventory.add(Orange.builder().weight(500).color(Color.RED).build());
+        inventory.add(Orange.builder().weight(300).color(Color.GREEN).build());
+    }
+
+    private static void prettyPrintApple(List<Orange> inventory, OrangeFormatter orangeFormatter){
+        for (Orange orange : inventory){
+            String output = orangeFormatter.accept(orange);
+            System.out.println(output);
+        }
     }
 }
