0472695fe08f9f995b3652cb20b9d9cb846533cc
Yahya <yahyahussein91@gmail.com>
2023-08-22 21:42:12 -0700

    predicates

--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/functionalinterfaces/Examples.java
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/functionalinterfaces/Examples.java
@@ -1,6 +1,12 @@
 package org.cydeo.functionalinterfaces;
 
+import java.util.function.Predicate;
+
 public class Examples {
+    public static void main(String[] args) {
 
-
+        Predicate<Integer> lesserThan = a -> a < 18;
+        Boolean result = lesserThan.test(50);
+        System.out.println(result);
+    }
 }
