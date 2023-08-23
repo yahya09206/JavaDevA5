e86642b115c242adf27597e66315ebda211962fd
Yahya <yahyahussein91@gmail.com>
2023-08-22 22:03:03 -0700

    printing users using lambda

--- JavaProgammingRefresh/lambda/src/main/java/org/cydeo/functionalinterfaces/UserTest.java
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/functionalinterfaces/UserTest.java
@@ -1,12 +1,21 @@
 package org.cydeo.functionalinterfaces;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class UserTest {
     public static void main(String[] args) {
 
+        List<User> users = new ArrayList<>();
+        users.add(User.builder().firstName("Mike").lastName("Smith").age(32).build());
+        users.add(User.builder().firstName("Mitch").lastName("Samuel").age(23).build());
+        users.add(User.builder().firstName("Meech").lastName("Sands").age(44).build());
+        users.add(User.builder().firstName("Meeks").lastName("Sally").age(51).build());
         // Print all elements in the list
+        printName(users, p -> true);
+        // Print all users that last name starts with E
+
     }
 
     private static void printName(List<User> users, Predicate<User> p){
