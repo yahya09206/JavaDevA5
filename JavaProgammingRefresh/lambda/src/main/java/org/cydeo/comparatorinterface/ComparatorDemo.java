8f526746c54882e610d792e95a93cc82b6fed102
Yahya <yahyahussein91@gmail.com>
2023-08-23 21:36:01 -0700

    descending order using comparator

---
+++ JavaProgammingRefresh/lambda/src/main/java/org/cydeo/comparatorinterface/ComparatorDemo.java
@@ -0,0 +1,30 @@
+package org.cydeo.comparatorinterface;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ComparatorDemo {
+    public static void main(String[] args) {
+
+        List<Integer> list = Arrays.asList(10, 0, 15, 5, 20);
+        System.out.println(list);
+
+        Collections.sort(list);
+        System.out.println(list);
+
+        // Descending order
+        Collections.sort(list, new MyComparator());
+        System.out.println(list);
+
+        Collections.sort(list,((o1,o2) -> (o1>o2) ? -1 : (o1<o2) ? 1:0));
+
+        //Ascending
+        list.sort((o1,o2) -> o1.compareTo(o2));
+        System.out.println(list);
+
+        // Descending
+        list.sort((o2,o1) -> o1.compareTo(o2));
+        System.out.println(list);
+    }
+}
