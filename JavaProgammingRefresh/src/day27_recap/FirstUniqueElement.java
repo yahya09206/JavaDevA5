1c0fc53e5fb68c1f27dde6c15000f6d190055740
Yahya <yahyahussein91@gmail.com>
2023-09-12 23:57:01 -0700

    done for night

---
+++ JavaProgammingRefresh/src/day27_recap/FirstUniqueElement.java
@@ -0,0 +1,9 @@
+package day27_recap;
+
+public class FirstUniqueElement {
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 2, 3, 3, 4};
+        // store first unique element in variable
+    }
+}
