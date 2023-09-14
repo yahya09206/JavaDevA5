19615203325e422dd151f3d219592d2a774019e9
Yahya <yahyahussein91@gmail.com>
2023-09-13 22:27:03 -0700

    converted to for each loop

--- JavaProgammingRefresh/src/day27_recap/FirstUniqueElement.java
+++ JavaProgammingRefresh/src/day27_recap/FirstUniqueElement.java
@@ -5,5 +5,39 @@ public class FirstUniqueElement {
 
         int[] nums = {1, 1, 2, 3, 3, 4};
         // store first unique element in variable
+        int firstUniqueElement = 0;
+
+
+        // outer loop to keep repeating inner loop
+//        for (int j = 0; j <= nums.length - 1; j++){
+//            // inner loop to count freq of one number
+//            int frequency = 0;
+//            for (int i = 0; i <= nums.length - 1; i++){
+//                if (nums[i] == nums[j]){
+//                    frequency++;
+//                }
+//            }
+//
+//            if (frequency == 1){
+//                firstUniqueElement = nums[j];
+//            }
+//        }
+
+        // using for each loop
+        for (int each : nums){
+            // inner loop to count freq of one number
+            int frequency = 0;
+            for (int element : nums){
+                if (element == each){
+                    frequency++;
+                }
+            }
+
+            if (frequency == 1){
+                firstUniqueElement = each;
+            }
+        }
+
+        System.out.println("firstUniqueElement = " + firstUniqueElement);
     }
 }
