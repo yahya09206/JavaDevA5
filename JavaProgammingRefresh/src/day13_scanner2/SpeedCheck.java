ee5553ae506e7f73eed5fa1377a4f6974a073780
Yahya <yahyahussein91@gmail.com>
2023-11-09 15:25:39 -0800

    speed limit task

---
+++ JavaProgammingRefresh/src/day13_scanner2/SpeedCheck.java
@@ -0,0 +1,13 @@
+package day13_scanner2;
+
+import java.util.Scanner;
+
+public class SpeedCheck {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int speedLimit = 35;
+        System.out.println("Enter your current speed");
+        int currentSpeed = scan.nextInt();
+    }
+}
