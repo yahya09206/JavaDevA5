ba49e18aac64deebe453522a5bd4c034520acd00
Yahya <yahyahussein91@gmail.com>
2023-11-09 15:06:33 -0800

    cents to dollars task

--- JavaProgammingRefresh/src/day13_scanner2/CentsToDollars.java
+++ JavaProgammingRefresh/src/day13_scanner2/CentsToDollars.java
@@ -8,6 +8,9 @@ public class CentsToDollars {
         // import scanner
         Scanner scan = new Scanner(System.in);
 
+        // Cents to Dollars
+        System.out.println("Enter Cents");
+        int centAmount = scan.nextInt();
 
     }
 }
