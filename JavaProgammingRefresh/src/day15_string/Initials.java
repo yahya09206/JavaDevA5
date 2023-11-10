eb5fc98040ef86c8f39bc188e84765adc7c7abbb
Yahya <yahyahussein91@gmail.com>
2023-11-10 15:20:25 -0800

    simple calculator

--- JavaProgammingRefresh/src/day15_string/Initials.java
+++ JavaProgammingRefresh/src/day15_string/Initials.java
@@ -1,8 +1,22 @@
 package day15_string;
 
+import java.util.Scanner;
+
 public class Initials {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter your first name:");
+        String firstName = scan.next();
+
+        System.out.println("Enter your last name:");
+        String lastName = scan.next();
+        scan.close();
+
+        char f = firstName.charAt(0);
+        char l = lastName.charAt(0);
+
+        System.out.println(f + "." + l);
     }
 }
---
+++ JavaProgammingRefresh/src/day15_string/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package day15_string;
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter first Number:");
+        double num1 = scan.nextDouble();
+    }
+}
