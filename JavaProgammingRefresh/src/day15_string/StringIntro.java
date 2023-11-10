5d4f963dbf52c13fce8bcf9da7e29d69b78ca84e
Yahya <yahyahussein91@gmail.com>
2023-11-10 14:22:38 -0800

    going thru string literals and objects

--- JavaProgammingRefresh/src/day15_string/StringIntro.java
+++ JavaProgammingRefresh/src/day15_string/StringIntro.java
@@ -6,5 +6,7 @@ public class StringIntro {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        String str = "Cybertek";
+        String str2 = new String("Cybertek");
     }
 }
