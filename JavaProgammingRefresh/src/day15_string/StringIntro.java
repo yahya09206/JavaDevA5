fba34de78cd0997b39bb1f890aa7499ecdf097b2
Yahya <yahyahussein91@gmail.com>
2023-11-10 14:36:17 -0800

    creating string object

--- JavaProgammingRefresh/src/day15_string/StringIntro.java
+++ JavaProgammingRefresh/src/day15_string/StringIntro.java
@@ -8,5 +8,22 @@ public class StringIntro {
         Scanner scan = new Scanner(System.in);
         String str = "Cybertek";
         String str2 = new String("Cybertek");
+
+        System.out.println("-----------------------------------");
+
+        String s1 = "Cat";
+        String s2 = "Cat";
+        String s3 = "Cat";
+
+        System.out.println(s1 == s3);
+
+
+        System.out.println("-----------------------------------");
+
+        String s4 = new String("Dog");
+        String s5 = new String("Dog");
+        System.out.println(s4 == s5);
+
+
     }
 }
