fdee500a430eefefe965f8237c1919780406fb16
Yahya <yahyahussein91@gmail.com>
2023-11-10 15:10:46 -0800

    string methods

---
+++ JavaProgammingRefresh/src/day15_string/Initials.java
@@ -0,0 +1,8 @@
+package day15_string;
+
+public class Initials {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ JavaProgammingRefresh/src/day15_string/StringMethods.java
@@ -0,0 +1,17 @@
+package day15_string;
+
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String str = "Muhtar";
+
+        char ch = str.charAt(0);
+
+        System.out.println("ch = " + ch);
+
+        System.out.println("-----------------------------------");
+
+
+
+    }
+}
