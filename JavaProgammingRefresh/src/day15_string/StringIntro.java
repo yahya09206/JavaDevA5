e5d24b913b43758bae5840bf009a9d35ca086a69
Yahya <yahyahussein91@gmail.com>
2023-11-10 14:15:56 -0800

    day 1 string refresh

---
+++ JavaProgammingRefresh/src/day15_string/StringIntro.java
@@ -0,0 +1,10 @@
+package day15_string;
+
+import java.util.Scanner;
+
+public class StringIntro {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+    }
+}
