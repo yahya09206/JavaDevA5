a55d492e0d5a8e7b0dbfeb95554a0b6a044b55df
Yahya <yahyahussein91@gmail.com>
2023-11-10 15:23:42 -0800

    setting up conditions with if statement

--- JavaProgammingRefresh/src/day15_string/SimpleCalculator.java
+++ JavaProgammingRefresh/src/day15_string/SimpleCalculator.java
@@ -9,5 +9,29 @@ public class SimpleCalculator {
 
         System.out.println("Enter first Number:");
         double num1 = scan.nextDouble();
+
+        System.out.println("Enter a math operator");
+        char mathOperator = scan.next().charAt(0);
+
+        System.out.println("Enter your second number");
+        double num2 = scan.nextDouble();
+
+        double result = 0;
+        switch (mathOperator){
+
+            case '-':
+                result = num1 - num2;
+                break;
+            case '+':
+                result = num1 + num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+        }
     }
 }
