07eeea808400baf4fe04e796740b5c68cdc916db
Yahya <yahyahussein91@gmail.com>
2023-11-10 15:46:08 -0800

    substring method

---
+++ JavaProgammingRefresh/src/day15_string/StringMethods2.java
@@ -0,0 +1,18 @@
+package day15_string;
+
+public class StringMethods2 {
+    public static void main(String[] args) {
+
+        String sentence = "Java Programming Language";
+        String name = sentence.substring(0, 3 + 1);
+        System.out.println(name);
+
+        String s2 = "Cybertek School";
+
+        String name2 = s2.substring(0, 7 + 1);
+        System.out.println(name2);
+
+        String name3 = s2.substring(9, 15);
+        System.out.println(name3);
+    }
+}
