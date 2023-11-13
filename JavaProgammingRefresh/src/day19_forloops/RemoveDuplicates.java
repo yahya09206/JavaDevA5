40d22acb181b1f3cc79be151af3684afe175c901
Yahya <yahyahussein91@gmail.com>
2023-11-13 06:57:15 -0800

    removing duplicates

---
+++ JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+package day19_forloops;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        String word = "ababababab";
+        String result = "";
+
+
+    }
+}
