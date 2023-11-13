738171944d48e62b54681de8e4e508c83a1c662d
Yahya <yahyahussein91@gmail.com>
2023-11-13 06:49:39 -0800

    for loop intro

---
+++ JavaProgammingRefresh/src/day19_forloops/ForLoopIntro.java
@@ -0,0 +1,10 @@
+package day19_forloops;
+
+public class ForLoopIntro {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 5; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
