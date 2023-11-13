f23cc2c2570204ddf0a1b455d04521bde1601808
Yahya <yahyahussein91@gmail.com>
2023-11-13 13:45:01 -0800

    arrays for days of the week

---
+++ JavaProgammingRefresh/src/day23_arrays/Days.java
@@ -0,0 +1,9 @@
+package day23_arrays;
+
+public class Days {
+    public static void main(String[] args) {
+
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    }
+}
