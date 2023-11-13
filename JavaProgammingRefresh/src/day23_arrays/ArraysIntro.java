c9b1c5aa898f38b326d8511bec789bfaf00f1eba
Yahya <yahyahussein91@gmail.com>
2023-11-13 13:35:45 -0800

    printing out array of names using for loop

--- JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
+++ JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
@@ -5,5 +5,17 @@ public class ArraysIntro {
 
         String[] group1 = {"Feruza", "Yulian", "Tamara", "Ahmet"};
 
+        // retrieve data from array
+        System.out.println(group1[0]);
+        System.out.println(group1[1]);
+        System.out.println(group1[2]);
+        System.out.println(group1[3]);
+
+        System.out.println("----------------------------------------------");
+
+        for (int i = 0; i <= group1.length - 1; i++){
+            System.out.println(group1[i]);
+        }
+
     }
 }
