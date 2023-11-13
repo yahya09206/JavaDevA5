48a9cfbbc25e3a5ae05b7cbce4d0d8569aa61d34
Yahya <yahyahussein91@gmail.com>
2023-11-13 13:49:57 -0800

    declaring array using curly brace notation

---
+++ JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
@@ -0,0 +1,10 @@
+package day23_arrays;
+
+public class MyClassMates {
+    public static void main(String[] args) {
+
+        String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
+
+        String[] friends2 = new String[5];
+    }
+}
