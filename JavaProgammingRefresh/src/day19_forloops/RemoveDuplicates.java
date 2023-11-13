d741cdddc2fbb40a8a32c8ac9cd2715943abb48f
Yahya <yahyahussein91@gmail.com>
2023-11-13 06:59:38 -0800

    check for dup chars

--- JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
+++ JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
@@ -6,6 +6,13 @@ public class RemoveDuplicates {
         String word = "ababababab";
         String result = "";
 
+        for (int i = 0; i <= word.length() - 1; i++) {
 
+            if (!result.contains("" + word.charAt(i))){
+                result += word.charAt(i);
+            }
+        }
+
+        System.out.println("result = " + result);
     }
 }
