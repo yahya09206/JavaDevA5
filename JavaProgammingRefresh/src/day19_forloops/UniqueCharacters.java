c60ecbe941f21ab4f3c9e0621e4e4fee22f5ac29
Yahya <yahyahussein91@gmail.com>
2023-11-13 07:09:30 -0800

    unique char with loop

--- JavaProgammingRefresh/src/day19_forloops/UniqueCharacters.java
+++ JavaProgammingRefresh/src/day19_forloops/UniqueCharacters.java
@@ -4,8 +4,21 @@ public class UniqueCharacters {
     public static void main(String[] args) {
 
         String word = "aabcc";
-        boolean isUnique = word.indexOf('a') == word.lastIndexOf('a');
+        String result = "";
 
-        System.out.println("isUnique = " + isUnique);
+
+        for (int i = 0; i <= word.length() - 1; i++){
+            char ch = word.charAt(i);
+            boolean isUnique = word.indexOf(ch) == word.lastIndexOf(ch);
+
+            if (isUnique){
+                result += ch;
+            }
+        }
+
+        System.out.println("result = " + result);
+//        boolean isUnique = word.indexOf('a') == word.lastIndexOf('a');
+//
+//        System.out.println("isUnique = " + isUnique);
     }
 }
