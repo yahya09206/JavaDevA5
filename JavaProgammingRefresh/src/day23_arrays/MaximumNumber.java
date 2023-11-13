48411294b7508aefac5a89d5f55cd0769348e87f
Yahya <yahyahussein91@gmail.com>
2023-11-13 14:07:42 -0800

    added check for minimum number

--- JavaProgammingRefresh/src/day23_arrays/MaximumNumber.java
+++ JavaProgammingRefresh/src/day23_arrays/MaximumNumber.java
@@ -5,12 +5,19 @@ public class MaximumNumber {
 
         int[] numbers = {10, 5, 4, 400, 50, 0, -1};
         int max = numbers[0];
+        int min = numbers[0];
 
         for (int i = 0; i <= numbers.length - 1; i++){
             int each = numbers[i];
             if (each > max){ // if any greater number is checked
                 max = each;
             }
+            if (each < max){ // if any lesser number is checked
+                min = each;
+            }
         }
+
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
     }
 }
