3104a6115d5405452a668290ebce7f04877773f2
Yahya <yahyahussein91@gmail.com>
2023-11-13 06:53:18 -0800

    set up loop for reversed word

--- JavaProgammingRefresh/src/day19_forloops/Palindrome.java
+++ JavaProgammingRefresh/src/day19_forloops/Palindrome.java
@@ -1,8 +1,16 @@
 package day19_forloops;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
 
+        String word = new Scanner(System.in).next();
+        String reversedWord = "";
+
+        for (int i = word.length(); i >= 0; i--) {
+            word.charAt(i);
+        }
 
     }
 }
