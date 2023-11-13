3255199d51efc524029618a3dd6823de28df6834
Yahya <yahyahussein91@gmail.com>
2023-11-13 13:56:01 -0800

    replaced nulls with actual names

--- JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
+++ JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
@@ -6,5 +6,16 @@ public class MyClassMates {
         String[] friends = {"Sabit", "Ahmet", "Tugba", "Beryl", "Ahmet"};
 
         String[] friends2 = new String[5];
+        System.out.println("Size: " + friends2.length);
+        friends2[0 ] = "Sabit";
+        friends2[1 ] = "Ahmet";
+        friends2[2 ] = "Tugba";
+        friends2[3 ] = "Beryl";
+        friends2[4 ] = "Ahmet";
+
+
+        for (int i = 0; i <= friends2.length - 1; i++){
+            System.out.println(friends2[i]);
+        }
     }
 }
