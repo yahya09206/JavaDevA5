5f5c594d095008091d820b973d2190d9158650c0
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:20:50 -0800

    print first 3 chars from each string in array

--- JavaProgammingRefresh/src/day24_arrays/ClassMates.java
+++ JavaProgammingRefresh/src/day24_arrays/ClassMates.java
@@ -1,8 +1,16 @@
 package day24_arrays;
 
+import java.util.Arrays;
+
 public class ClassMates {
     public static void main(String[] args) {
 
         String[] classmates = {"Gulistan", "Feruza", "Ahmet", "Memet"};
+
+        System.out.println("Arrays.toString(classmates) = " + Arrays.toString(classmates));
+
+            for (int i = 0; i <= classmates.length - 1; i++) {
+                System.out.println(classmates[i].substring(0, 3));
+        }
     }
 }
