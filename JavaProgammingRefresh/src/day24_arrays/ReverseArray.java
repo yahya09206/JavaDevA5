cb7088904e1e827f057a7a344200aca9785cb356
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:44:01 -0800

    storing reversed values of old array into newer array and moving

--- JavaProgammingRefresh/src/day24_arrays/ReverseArray.java
+++ JavaProgammingRefresh/src/day24_arrays/ReverseArray.java
@@ -6,8 +6,8 @@ public class ReverseArray {
         int[] array = {1, 2, 3, 4, 5};
         int[] reversedArray = new int[array.length];
 
-        for (int i = reversedArray.length - 1; i >= 0; i--){
-
+        for (int i = array.length - 1, j = 0; i >= 0; i--, j++){
+            reversedArray[j] = array[i];
         }
     }
 }
