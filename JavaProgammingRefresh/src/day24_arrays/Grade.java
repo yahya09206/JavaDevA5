e44504eb4a0267fa13f82dbeac8e82a0f66832cd
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:26:52 -0800

    set up grade check for one student

--- JavaProgammingRefresh/src/day24_arrays/Grade.java
+++ JavaProgammingRefresh/src/day24_arrays/Grade.java
@@ -6,5 +6,18 @@ public class Grade {
         String[] names = {"Anna", "Nancy", "Sarah"};
         int[] scores = {90, 75, 80};
         char[] grades = new char[names.length];
+
+        int score = scores[i];
+        if (scores[0] >= 90){
+            grades[0] = 'A';
+        }else if (scores[0] >= 80){
+            grades[0] = 'B';
+        }else if (scores[0] >= 70){
+            grades[0] = 'C';
+        } else if (scores[0] >= 60) {
+            grades[0] = 'D';
+        }else {
+            grades[0] = 'F';
+        }
     }
 }
