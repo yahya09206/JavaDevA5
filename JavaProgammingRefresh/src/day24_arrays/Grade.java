2204ab7eb336372b261c1ae138f1297e3067f6db
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:23:22 -0800

    printing names with grades

---
+++ JavaProgammingRefresh/src/day24_arrays/Grade.java
@@ -0,0 +1,10 @@
+package day24_arrays;
+
+public class Grade {
+    public static void main(String[] args) {
+
+        String[] names = {"Anna", "Nancy", "Sarah"};
+        int[] scores = {90, 75, 80};
+        char[] grades = new char[names.length];
+    }
+}
