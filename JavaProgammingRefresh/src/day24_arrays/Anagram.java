0755948ec8409d3543702ad939eba6d49dbe4ea6
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:57:23 -0800

    was using wrong equals method

--- JavaProgammingRefresh/src/day24_arrays/Anagram.java
+++ JavaProgammingRefresh/src/day24_arrays/Anagram.java
@@ -1,4 +1,21 @@
 package day24_arrays;
 
+import java.util.Arrays;
+
 public class Anagram {
+    public static void main(String[] args) {
+
+        String word1 = "heart";
+        String word2 = "earth";
+
+        char[] str1 = word1.toCharArray();
+        char[] str2 = word2.toCharArray();
+
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        boolean isAnagram = Arrays.equals(str1, str2);
+
+        System.out.println("isAnagram = " + isAnagram);
+    }
 }
