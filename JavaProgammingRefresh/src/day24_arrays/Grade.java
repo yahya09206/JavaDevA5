0a5c1e9d3b1fcf886080c18e7cb3ff6e362d22a3
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:30:27 -0800

    print statement

--- JavaProgammingRefresh/src/day24_arrays/Grade.java
+++ JavaProgammingRefresh/src/day24_arrays/Grade.java
@@ -8,18 +8,19 @@ public class Grade {
         char[] grades = new char[names.length];
 
         for (int i = 0; i <= grades.length - 1; i++){
-            int score = scores[i];
-            if (scores[0] >= 90){
-                grades[0] = 'A';
-            }else if (scores[0] >= 80){
-                grades[0] = 'B';
-            }else if (scores[0] >= 70){
-                grades[0] = 'C';
-            } else if (scores[0] >= 60) {
-                grades[0] = 'D';
+            if (scores[i] >= 90){
+                grades[i] = 'A';
+            }else if (scores[i] >= 80){
+                grades[i] = 'B';
+            }else if (scores[i] >= 70){
+                grades[i] = 'C';
+            } else if (scores[i] >= 60) {
+                grades[i] = 'D';
             }else {
-                grades[0] = 'F';
+                grades[i] = 'F';
             }
+
+            System.out.println(names[i] + " ' score is " + scores[i] + ", made " + grades[i]);
         }
     }
 }
