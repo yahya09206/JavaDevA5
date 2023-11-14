3a31102a3e4be0170b7d78d5fd73f6cd615d58a2
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:35:02 -0800

    checking for equality and incrementing count if found

--- JavaProgammingRefresh/src/day24_arrays/FrequencyOfElements.java
+++ JavaProgammingRefresh/src/day24_arrays/FrequencyOfElements.java
@@ -4,5 +4,14 @@ public class FrequencyOfElements {
     public static void main(String[] args) {
 
         int[] arr = {1, 1, 2, 3, 3, 4};
+
+
+        int element = arr[0];
+        int count = 0;
+        for (int i = 0; i <= arr.length - 1; i++){
+            if (arr[i] == element){
+                count++;
+            }
+        }
     }
 }
