a837e723963e8b8fe08e87498ad34da20a65e05d
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:48:23 -0800

    string array methods

---
+++ JavaProgammingRefresh/src/day24_arrays/StringMethods.java
@@ -0,0 +1,9 @@
+package day24_arrays;
+
+public class StringMethods {
+    public static void main(String[] args) {
+
+        String sentence = "I love learning java programming language";
+        String[] words = sentence.split(" ");
+    }
+}
