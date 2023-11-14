7a65ccad21ce7eed1dcf0b1e088dd4b1c129244c
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:37:37 -0800

    adding outer loop to go thru numbers

--- JavaProgammingRefresh/src/day24_arrays/FrequencyOfElements.java
+++ JavaProgammingRefresh/src/day24_arrays/FrequencyOfElements.java
@@ -6,16 +6,8 @@ public class FrequencyOfElements {
         int[] arr = {1, 1, 2, 3, 3, 4};
 
 
-        int element = arr[0];
-        int count = 0;
-        for (int i = 0; i <= arr.length - 1; i++){
-            if (arr[i] == element){
-                count++;
-            }
-        }
+        for (int j = 0; j <= arr.length - 1; j++){
 
-        if (count == 1){
-            System.out.println(element);
         }
     }
 }
