2b4960522ab6bd7c2e083dfce6edf4e56cc1b00a
Yahya <yahyahussein91@gmail.com>
2023-11-14 14:27:22 -0800

    wrapped grade checks in loop

--- JavaProgammingRefresh/src/day24_arrays/Grade.java
+++ JavaProgammingRefresh/src/day24_arrays/Grade.java
@@ -7,17 +7,19 @@ public class Grade {
         int[] scores = {90, 75, 80};
         char[] grades = new char[names.length];
 
-        int score = scores[i];
-        if (scores[0] >= 90){
-            grades[0] = 'A';
-        }else if (scores[0] >= 80){
-            grades[0] = 'B';
-        }else if (scores[0] >= 70){
-            grades[0] = 'C';
-        } else if (scores[0] >= 60) {
-            grades[0] = 'D';
-        }else {
-            grades[0] = 'F';
+        for (int i = 0; i <= grades.length - 1; i++){
+            int score = scores[i];
+            if (scores[0] >= 90){
+                grades[0] = 'A';
+            }else if (scores[0] >= 80){
+                grades[0] = 'B';
+            }else if (scores[0] >= 70){
+                grades[0] = 'C';
+            } else if (scores[0] >= 60) {
+                grades[0] = 'D';
+            }else {
+                grades[0] = 'F';
+            }
         }
     }
 }
