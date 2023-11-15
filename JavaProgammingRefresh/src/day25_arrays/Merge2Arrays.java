fd6c4889303d042172e5dda7350c1ac6297ff35c
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:35:14 -0800

    setting up new array as length of first two

---
+++ JavaProgammingRefresh/src/day25_arrays/Merge2Arrays.java
@@ -0,0 +1,10 @@
+package day25_arrays;
+
+public class Merge2Arrays {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 3, 4},
+                arr2 = {5, 6};
+        int[] arr3 = new int[arr1.length + arr2.length];
+    }
+}
