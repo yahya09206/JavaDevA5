b8db0a38b735459111e15dd1e623e25d24a5479e
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:55:42 -0800

    using foreach to reverse elemnts

---
+++ JavaProgammingRefresh/src/day25_arrays/ReverseElements2.java
@@ -0,0 +1,12 @@
+package day25_arrays;
+
+public class ReverseElements2 {
+    public static void main(String[] args) {
+
+        String[] array = {"Java", "Python", "C#", "Racecar"};
+
+        for (String elements : array) {
+            String reverse = "";
+        }
+    }
+}
