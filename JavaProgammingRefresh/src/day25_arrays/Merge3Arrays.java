16b756c28ad26800fce95d447e2833392a4062da
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:43:14 -0800

    was able to merge all three arrays into fourth one

--- JavaProgammingRefresh/src/day25_arrays/Merge3Arrays.java
+++ JavaProgammingRefresh/src/day25_arrays/Merge3Arrays.java
@@ -1,5 +1,7 @@
 package day25_arrays;
 
+import java.util.Arrays;
+
 public class Merge3Arrays {
     public static void main(String[] args) {
 
@@ -14,12 +16,14 @@ public class Merge3Arrays {
             arr4[j++] = arr1[i];
         }
 
-        for (int i = 0; i <= arr1.length - 1; i++){
-            arr4[j++] = arr1[i];
+        for (int i = 0; i <= arr2.length - 1; i++){
+            arr4[j++] = arr2[i];
         }
 
-        for (int i = 0; i <= arr1.length - 1; i++){
-            arr4[j++] = arr1[i];
+        for (int i = 0; i <= arr3.length - 1; i++){
+            arr4[j++] = arr3[i];
         }
+
+        System.out.println(Arrays.toString(arr4));
     }
 }
