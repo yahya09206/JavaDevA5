4903e5d75ec92a8dbcfb15e1ed1c4ea39698053c
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:38:00 -0800

    merged first array into third array

--- JavaProgammingRefresh/src/day25_arrays/Merge2Arrays.java
+++ JavaProgammingRefresh/src/day25_arrays/Merge2Arrays.java
@@ -1,10 +1,19 @@
 package day25_arrays;
 
+import java.util.Arrays;
+
 public class Merge2Arrays {
     public static void main(String[] args) {
 
         int[] arr1 = {1, 2, 3, 4},
                 arr2 = {5, 6};
         int[] arr3 = new int[arr1.length + arr2.length];
+
+        int j = 0;
+        for (int i = 0; i <= arr1.length - 1; i++){
+            arr3[j++] = arr1[i];
+        }
+
+        System.out.println(Arrays.toString(arr3));
     }
 }
