9263b64db551a644028c9a2af53398f0ed9f5e2d
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:50:00 -0800

    merging first array without index

--- JavaProgammingRefresh/src/day25_arrays/ForeachLoopPractice.java
+++ JavaProgammingRefresh/src/day25_arrays/ForeachLoopPractice.java
@@ -4,5 +4,14 @@ public class ForeachLoopPractice {
     public static void main(String[] args) {
 
 
+        String[] arr1 = {"A", "B", "C", "D"},
+                arr2 = {"E", "F"};
+
+        String[] arr3 = new String[arr1.length + arr2.length];
+
+        int j = 0;
+        for (String each : arr1) {
+            arr3[j++] = each;
+        }
     }
 }
