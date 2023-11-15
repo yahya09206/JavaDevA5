85da50cdd224ee51be141b6d236faa17ef83f848
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:28:01 -0800

    forgot to add inside loop

--- JavaProgammingRefresh/src/day25_arrays/ReverseElements.java
+++ JavaProgammingRefresh/src/day25_arrays/ReverseElements.java
@@ -1,15 +1,22 @@
 package day25_arrays;
 
+import java.util.Arrays;
+
 public class ReverseElements {
     public static void main(String[] args) {
 
         String[] array = {"Java", "Python", "C#"};
 
-        String element = array[0];
-        String reverse = "";
-
-        for (int i = element.length() - 1; i >= 0; i--){
-            reverse += element.charAt(i);
+        for (int j = 0; j <= array.length - 1; j++){
+            String element = array[j];
+            String reverse = "";
+            for (int i = element.length() - 1; i >= 0; i--){
+                reverse += element.charAt(i);
+            }
+            array[j] = reverse;
         }
+
+        System.out.println(Arrays.toString(array));
+
     }
 }
