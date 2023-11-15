6b3057bc0e4a684892ddd83b75cb818a4d45b87d
Yahya <yahyahussein91@gmail.com>
2023-11-15 13:41:01 -0800

    merging 3 arrays

---
+++ JavaProgammingRefresh/src/day25_arrays/Merge3Arrays.java
@@ -0,0 +1,14 @@
+package day25_arrays;
+
+public class Merge3Arrays {
+    public static void main(String[] args) {
+
+        int[] arr1 = {1, 2, 3},
+                arr2 = {4, 5, 6},
+                arr3 = {7, 8, 9, 12, 13, 14};
+
+        int[] arr4 = new int[arr1.length + arr2.length + arr3.length];
+
+
+    }
+}
