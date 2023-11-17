c2ec5094ef92b3b8422e8db24097f749f4a35afc
Yahya <yahyahussein91@gmail.com>
2023-11-16 19:53:04 -0800

    reversing int array

--- JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask1.java
+++ JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask1.java
@@ -5,5 +5,12 @@ public class WarmupTask1 {
 
     public static int[] reverse(int[] array){
 
+        int[] result = new int[array.length];
+
+        for (int i = array.length - 1, j = 0; i >= 0; i--){
+            result[j] = array[i];
+        }
+
+        return result;
     }
 }
