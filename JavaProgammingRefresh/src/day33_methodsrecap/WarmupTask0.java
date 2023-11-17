737aca2239fa09ea0e0356001cc3df9c717a0fcc
Yahya <yahyahussein91@gmail.com>
2023-11-16 19:31:20 -0800

    fixed check for index lenght

--- JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask0.java
+++ JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask0.java
@@ -1,14 +1,20 @@
 package day33_methodsrecap;
 
+import java.util.Arrays;
+
 public class WarmupTask0 {
     public static void main(String[] args) {
 
         // remove element at given index of the array
+        int[] numbers = {100, 200, 300, 400, 500, 600};
+        numbers = remove(numbers, 3);
+
+        System.out.println("Arrays.toString(numbers) = " + Arrays.toString(numbers));
     }
 
     public static int[] remove(int[] array, int index) {
 
-        if (index >= 0 && index <= array.length - 1) {
+        if (index >= 0 || index > array.length - 1) {
 
             System.err.println("Invalid index");
             return array;
