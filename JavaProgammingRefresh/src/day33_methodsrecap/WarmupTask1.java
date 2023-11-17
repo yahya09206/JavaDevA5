6eaef195461dbcf6c9b01357e773eaf3904d30f5
Yahya <yahyahussein91@gmail.com>
2023-11-16 19:55:18 -0800

    reversing char array

--- JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask1.java
+++ JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask1.java
@@ -25,5 +25,27 @@ public class WarmupTask1 {
         return result;
     }
 
+    public static double[] reverse(double[] array){
+
+        double[] result = new double[array.length];
+
+        for (int i = array.length - 1, j = 0; i >= 0; i--){
+            result[j] = array[i];
+        }
+
+        return result;
+    }
+
+    public static char[] reverse(char[] array){
+
+        char[] result = new char[array.length];
+
+        for (int i = array.length - 1, j = 0; i >= 0; i--){
+            result[j] = array[i];
+        }
+
+        return result;
+    }
+
 
 }
