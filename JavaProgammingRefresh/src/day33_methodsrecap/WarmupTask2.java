8ed36a602f874dd6b20ef94dd648a70951851b9d
Yahya <yahyahussein91@gmail.com>
2023-11-16 20:04:32 -0800

    overloaded contains method

---
+++ JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask2.java
@@ -0,0 +1,15 @@
+package day33_methodsrecap;
+
+public class WarmupTask2 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+    }
+
+    public static boolean contains(int[] array, int element){
+
+        boolean result = false;
+
+        return result;
+    }
+}
