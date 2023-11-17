f27059c5999cc4e7c024d2642c70f8fe5a613fb9
Yahya <yahyahussein91@gmail.com>
2023-11-16 19:22:58 -0800

    task to remove element at given index

---
+++ JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask0.java
@@ -0,0 +1,12 @@
+package day33_methodsrecap;
+
+public class WarmupTask0 {
+    public static void main(String[] args) {
+
+        // remove element at given index of the array
+    }
+
+    public static int[] remove(int[] array, int index){
+
+    }
+}
