b17f17fc3f510d4a350bb91ee2dbcd52cea59ebd
Yahya <yahyahussein91@gmail.com>
2023-11-16 20:24:30 -0800

    overloaded for unique elements

--- JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask3.java
+++ JavaProgammingRefresh/src/day33_methodsrecap/WarmupTask3.java
@@ -1,6 +1,70 @@
 package day33_methodsrecap;
 
+import utilities.ArraysUtility;
+
+import java.util.Arrays;
+
 public class WarmupTask3 {
+    public static void main(String[] args) {
+
+        int[] array = {1, 1, 1, 2, 3, 3, 4, 5, 5, 6};
+        int[] result = {};
+
+        for (int each : array) {
+            if (frequency(array, each) == 1){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] unique(int[] array){
+        int[] result = {};
+
+        for (int each : array) {
+            if (frequency(array, each) == 1){
+                result = ArraysUtility.addElement(result, each);
+            }
+        }
+        return result;
+    }
+
+    public static double[] unique(double[] array){
+        double[] result = {};
+
+        for (double each : array) { // getting each elements
+            if(  frequency(array, each)  == 1   ){ // unique element
+                result =  ArraysUtility.addElement(result, each );
+            }
+        }
+
+        return result;
+    }
+
+    public static String[] unique(String[] array){
+        String[] result = {};
+
+        for (String each : array) { // getting each elements
+            if(  frequency(array, each)  == 1   ){ // unique element
+                result =  ArraysUtility.addElement(result, each );
+            }
+        }
+
+        return result;
+    }
+
+    public static char[] unique(char[] array){
+        char[] result = {};
+
+        for (char each : array) { // getting each elements
+            if(  frequency(array, each)  == 1   ){ // unique element
+                result =  ArraysUtility.addElement(result, each );
+            }
+        }
+
+        return result;
+    }
 
 
     public static int frequency(int[] array, int element){
