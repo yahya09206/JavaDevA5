556451b6b95af896f65525bb4b6d802dd32fc991
Yahya <yahyahussein91@gmail.com>
2023-11-18 07:47:41 -0800

    practice with static instance variables

--- JavaProgammingRefresh/src/day39_static/StaticExamples.java
+++ JavaProgammingRefresh/src/day39_static/StaticExamples.java
@@ -1,14 +1,27 @@
 package day39_static;
 
 public class StaticExamples {
+    public static void main(String[] args) {
+
+        CybertekStudent student1 = new CybertekStudent();
+        CybertekStudent student2 = new CybertekStudent();
+        CybertekStudent student3 = new CybertekStudent();
+        CybertekStudent student4 = new CybertekStudent();
+        //student1.schoolName = "Cybertek School";
+
+        System.out.println(student1.schoolName);
+        System.out.println(student2.schoolName);
+        System.out.println(student3.schoolName);
+        System.out.println(student4.schoolName);
 
 
+    }
 }
 
 class CybertekStudent {
 
     public String name, gender;
     public int age, ID;
-
-    public String schoolName;
+    public static String schoolName = "Cybertek School";
+    public static String programmingLanguage = "Java";
 }
