51f2ad538cd3610f631c825c929ee03e37605c65
Yahya <yahyahussein91@gmail.com>
2023-11-18 08:36:18 -0800

    properly printing

--- JavaProgammingRefresh/src/day40_statics/Main.java
+++ JavaProgammingRefresh/src/day40_statics/Main.java
@@ -10,5 +10,22 @@ public class Main {
 
         System.out.println("Enter your building number");
         String building = scanner.next();
+
+        scanner.nextLine();
+        System.out.println("Enter your street name");
+        String street = scanner.nextLine();
+
+        System.out.println("Enter your city name");
+        String city = scanner.nextLine();
+
+        System.out.println("Enter your state name");
+        String state = scanner.nextLine();
+
+        System.out.println("Enter your zipcode");
+        int zipCode = scanner.nextInt();
+
+        address.setInfo(building, street, city, state, zipCode);
+
+        System.out.println(address);
     }
 }
