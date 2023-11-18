1060173dcd4d4252fc392c8f14d08bef584f4d0a
Yahya <yahyahussein91@gmail.com>
2023-11-18 08:25:39 -0800

    generated constructor

--- JavaProgammingRefresh/src/day40_statics/Address.java
+++ JavaProgammingRefresh/src/day40_statics/Address.java
@@ -2,6 +2,15 @@ package day40_statics;
 
 public class Address {
 
-    public String buildingNumber, street, city, state, zipCode;
+    public String buildingNumber, street, city, state;
+    public int zipCode;
     public static String country = "United States";
+
+    public Address(String buildingNumber, String street, String city, String state, int zipCode) {
+        this.buildingNumber = buildingNumber;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
 }
