0c9e735f0bfdb85c377b5c84f2edcff417b87d60
Yahya <yahyahussein91@gmail.com>
2023-11-18 07:12:31 -0800

    creating instances of dog

---
+++ JavaProgammingRefresh/src/day37_classintro/DogPark.java
@@ -0,0 +1,11 @@
+package day37_classintro;
+
+public class DogPark {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog();
+        dog1.name = "Mikey";
+
+        System.out.println(dog1.name);
+    }
+}
