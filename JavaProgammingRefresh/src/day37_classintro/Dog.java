5b2dc6912601865fa18789d89a1b7ab6ba051be2
Yahya <yahyahussein91@gmail.com>
2023-11-18 07:18:21 -0800

    set method for dog info

--- JavaProgammingRefresh/src/day37_classintro/Dog.java
+++ JavaProgammingRefresh/src/day37_classintro/Dog.java
@@ -25,4 +25,15 @@ public class Dog {
         System.out.println(name + " is playing");
     }
 
+    public void setInfo(String dogName, String dogBreed, String dogGender, String dogColor, String dogSize, int dogAge){
+
+        name = dogName;
+        breed = dogBreed;
+        gender = dogGender;
+        color = dogColor;
+        size = dogSize;
+        color = dogColor;
+
+    }
+
 }
