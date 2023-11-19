53ecc8cad9bc802c9febddebb4f6b8a28305ae71
Yahya <yahyahussein91@gmail.com>
2023-11-19 07:16:14 -0800

    inhertitance

---
+++ JavaProgammingRefresh/src/day45_Inheritance/Square.java
@@ -0,0 +1,13 @@
+package day45_Inheritance;
+
+public class Square {
+
+    private double side;
+    public double getSide(){
+        return side;
+    }
+
+    public void setSide(double side){
+        this.side = side;
+    }
+}
