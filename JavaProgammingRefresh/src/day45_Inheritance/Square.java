9a8cebb6b3f487e279f05b38036f0c85e306709e
Yahya <yahyahussein91@gmail.com>
2023-11-19 07:24:47 -0800

    constructing square class

--- JavaProgammingRefresh/src/day45_Inheritance/Square.java
+++ JavaProgammingRefresh/src/day45_Inheritance/Square.java
@@ -3,11 +3,19 @@ package day45_Inheritance;
 public class Square {
 
     private double side;
+
+    public Square(){
+        setSide(side);
+    }
     public double getSide(){
         return side;
     }
-
     public void setSide(double side){
+        if (side < 0){
+            return;
+        }
         this.side = side;
     }
+
+
 }
