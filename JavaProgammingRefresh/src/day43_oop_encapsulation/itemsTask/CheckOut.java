7f21bc70e90ce7ff928f668632ec9e238da3182f
Yahya <yahyahussein91@gmail.com>
2023-11-21 07:07:35 -0800

    changed to static import instead of calling mycart object

--- JavaProgammingRefresh/src/day43_oop_encapsulation/itemsTask/CheckOut.java
+++ JavaProgammingRefresh/src/day43_oop_encapsulation/itemsTask/CheckOut.java
@@ -3,16 +3,22 @@ package day43_oop_encapsulation.itemsTask;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static day43_oop_encapsulation.itemsTask.MyCart.*;
+
 public class CheckOut {
     public static void main(String[] args) {
 
         ArrayList<Item> items = new ArrayList<>();
         items.addAll(Arrays.asList(
-                MyCart.item1,
-                MyCart.item2,
-                MyCart.item3,
-                MyCart.item4,
-                MyCart.item5
+                item1,
+                item2,
+                item3,
+                item4,
+                item5
         ));
+
+        for (Item eachItem : items) {
+
+        }
     }
 }
