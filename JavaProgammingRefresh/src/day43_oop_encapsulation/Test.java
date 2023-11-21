51a5ff96203aeb6acf5c1e9084a0f59e55e4ca46
Yahya <yahyahussein91@gmail.com>
2023-11-21 07:43:33 -0800

    done with access modifiers

--- JavaProgammingRefresh/src/day43_oop_encapsulation/Test.java
+++ JavaProgammingRefresh/src/day43_oop_encapsulation/Test.java
@@ -5,6 +5,7 @@ public class Test {
 
         System.out.println(AccessModifiers.publicVariable); // reachable within same package
         System.out.println(AccessModifiers.defaultVariable);// reachable within same package
-        System.out.println(AccessModifiers.privateVariable); // not reachable within same package
+        // System.out.println(AccessModifiers.privateVariable); // not reachable within same package
+
     }
 }
