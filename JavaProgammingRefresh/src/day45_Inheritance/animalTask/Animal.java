80b0a13a20ad3dabfe268e027913ae1b953265ad
Yahya <yahyahussein91@gmail.com>
2023-11-23 15:59:50 -0800

    attributes that will be the same for each child class

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Animal.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Animal.java
@@ -3,5 +3,10 @@ package day45_Inheritance.animalTask;
 public class Animal {
 
     public String name;
+    public String breed;
+    public char gender;
+    public String size;
+    public int age;
+    public String color;
 
 }
