6959c3e87034c58c86083c0e86a41fd7de144111
Yahya <yahyahussein91@gmail.com>
2023-11-23 16:00:21 -0800

    generated constructor

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Animal.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Animal.java
@@ -9,4 +9,15 @@ public class Animal {
     public int age;
     public String color;
 
+    public Animal(String name, String breed, char gender, String size, int age, String color) {
+        this.name = name;
+        this.breed = breed;
+        this.gender = gender;
+        this.size = size;
+        this.age = age;
+        this.color = color;
+    }
+
+    public static boolean breathe = true;
+
 }
