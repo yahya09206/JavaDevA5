4195c1a9b1cae7b239997c684dab11b58257ccca
Yahya <yahyahussein91@gmail.com>
2023-11-23 16:26:14 -0800

    calling methods for newly created dog object

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Zoo.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Zoo.java
@@ -4,5 +4,10 @@ public class Zoo {
     public static void main(String[] args) {
 
 
+        Dog dog = new Dog("Lucy", "Bull Terrier", 'M', "Tiny", 2, "White");
+        dog.bark();
+        dog.eat("dog food");
+        dog.drink("water");
+        dog.sleep();
     }
 }
