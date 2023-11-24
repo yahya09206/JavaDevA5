067295ed7bad3ad05cbda34e531fe545e34c532c
Yahya <yahyahussein91@gmail.com>
2023-11-23 16:02:34 -0800

    set up methods to use

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Animal.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Animal.java
@@ -8,7 +8,7 @@ public class Animal {
     public String size;
     public int age;
     public String color;
-
+    public static boolean breathe = true;
     public Animal(String name, String breed, char gender, String size, int age, String color) {
         this.name = name;
         this.breed = breed;
@@ -18,6 +18,27 @@ public class Animal {
         this.color = color;
     }
 
-    public static boolean breathe = true;
+    public void eat(String food){
+        System.out.println(name + " is eating " + food);
+    }
 
+    public void drink(String drink){
+        System.out.println(name + " is drinking " + drink);
+    }
+
+    public void sleep(){
+        System.out.println(name + " is sleeping");
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                ", size='" + size + '\'' +
+                ", age=" + age +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
