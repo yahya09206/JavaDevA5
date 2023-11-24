7d7f9cefe29b8714da28523e049d6aa01f1f7a71
Yahya <yahyahussein91@gmail.com>
2023-11-23 16:22:18 -0800

    set up tiget class

---
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Tiger.java
@@ -0,0 +1,12 @@
+package day45_Inheritance.animalTask;
+
+public class Tiger extends Animal{
+
+    public Tiger(String name, String breed, char gender, String size, int age, String color){
+        setInfo(name, breed, gender, size, age, color);
+    }
+
+    public void roar(){
+        System.out.println(name + " is roaring");
+    }
+}
