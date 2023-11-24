de9cd65b48ad3a0644064d42591635a93cfa6b4c
Yahya <yahyahussein91@gmail.com>
2023-11-23 16:16:38 -0800

    set up instance method for cat class

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Cat.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Cat.java
@@ -2,5 +2,11 @@ package day45_Inheritance.animalTask;
 
 public class Cat extends Animal{
 
+    public Cat(String name, String breed, char gender, String size, int age, String color){
+        setInfo(name, breed, gender, size, age, color);
+    }
 
+    public void meow(){
+        System.out.println(name + " is meowing");
+    }
 }
