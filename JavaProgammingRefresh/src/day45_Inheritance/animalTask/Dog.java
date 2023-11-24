8435c5972a2ea8b121f1a0ba4648e2f7aeb25f9a
Yahya <yahyahussein91@gmail.com>
2023-11-23 16:08:05 -0800

    set info by calling set info method

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Dog.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Dog.java
@@ -3,6 +3,6 @@ package day45_Inheritance.animalTask;
 public class Dog extends Animal{
 
     public Dog(String name, String breed, char gender, String size, int age, String color){
-
+        setInfo(name, breed, gender, size, age, color);
     }
 }
