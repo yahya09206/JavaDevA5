1646314cdd454171ef367752ed2ea4f820b31f5f
Yahya <yahyahussein91@gmail.com>
2023-11-27 22:40:15 -0800

    done playing

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Zoo.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Zoo.java
@@ -3,7 +3,7 @@ package day45_Inheritance.animalTask;
 public class Zoo {
     public static void main(String[] args) {
 
-
+        // new instance of dog
         Dog dog = new Dog("Lucy", "Bull Terrier", 'M', "Tiny", 2, "White");
         dog.bark();
         dog.eat("dog food");
