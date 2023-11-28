b369c13b6bee33fe85384c6d435271b0ce62dea3
Yahya <yahyahussein91@gmail.com>
2023-11-28 15:30:23 -0800

    calling super constructor to child classes

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Dog.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Dog.java
@@ -2,9 +2,6 @@ package day45_Inheritance.animalTask;
 
 public class Dog extends Animal{
 
-    public Dog(String name, String breed, char gender, String size, int age, String color){
-        setInfo(name, breed, gender, size, age, color);
-    }
 
     public void bark(){
         System.out.println(name + " is barking");
