9a7fd9c766b90cc6fb55d7b2e460797ab3f470a8
Yahya <yahyahussein91@gmail.com>
2023-11-29 19:42:01 -0800

    got caught up with garbage

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Tiger.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Tiger.java
@@ -2,6 +2,7 @@ package day45_Inheritance.animalTask;
 
 public class Tiger extends Animal{
 
+    // called parent constructor
     public Tiger(String name, String breed, char gender, String size, int age, String color) {
         super(name, breed, gender, size, age, color);
     }
