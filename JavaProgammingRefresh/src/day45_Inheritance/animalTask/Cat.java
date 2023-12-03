344969faa60eeee5a3dd4c89ddc231239fe8280b
Yahya <yahyahussein91@gmail.com>
2023-12-02 23:11:52 -0800

    not looking too good

--- JavaProgammingRefresh/src/day45_Inheritance/animalTask/Cat.java
+++ JavaProgammingRefresh/src/day45_Inheritance/animalTask/Cat.java
@@ -2,6 +2,7 @@ package day45_Inheritance.animalTask;
 
 public class Cat extends Animal{
 
+    // cat constructor
     public Cat(String name, String breed, char gender, String size, int age, String color) {
         super(name, breed, gender, size, age, color);
     }
