243383169395900b345498605283f2a4e52f496a
Yahya <yahyahussein91@gmail.com>
2023-12-05 06:49:03 -0800

    created constructor

---
+++ JavaProgammingRefresh/src/day46_Inheritance/Car.java
@@ -0,0 +1,20 @@
+package day46_Inheritance;
+
+public class Car {
+
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private double miles;
+
+    public Car(String brand, String model, int year, String color, double miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.miles = miles;
+    }
+
+
+}
