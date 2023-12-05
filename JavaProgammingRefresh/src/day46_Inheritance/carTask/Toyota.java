36b8fb8e788aa86558c33ad2a010e3e3ba522807
Yahya <yahyahussein91@gmail.com>
2023-12-05 07:04:28 -0800

    added boolean variable

--- JavaProgammingRefresh/src/day46_Inheritance/carTask/Toyota.java
+++ JavaProgammingRefresh/src/day46_Inheritance/carTask/Toyota.java
@@ -2,7 +2,11 @@ package day46_Inheritance.carTask;
 
 public class Toyota extends Car{
 
+    public static boolean isReliable = true;
+
     public Toyota(String brand, String model, int year, double price, String color, long miles) {
         super(brand, model, year, price, color, miles);
     }
+
+
 }
