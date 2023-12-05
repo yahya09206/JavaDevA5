c772d83d8d52e853e85f1b475a9a21081e515a5f
Yahya <yahyahussein91@gmail.com>
2023-12-05 07:08:39 -0800

    removed brand

--- JavaProgammingRefresh/src/day46_Inheritance/carTask/BMW.java
+++ JavaProgammingRefresh/src/day46_Inheritance/carTask/BMW.java
@@ -2,7 +2,7 @@ package day46_Inheritance.carTask;
 
 public class BMW extends Car{
 
-    public BMW(String brand, String model, int year, double price, String color, long miles) {
-        super(brand, model, year, price, color, miles);
+    public BMW(String model, int year, double price, String color, long miles) {
+        super("BMW", model, year, price, color, miles);
     }
 }
