56b673ccb56c847febda5eb9355e8c6ec06aa9e3
Yahya <yahyahussein91@gmail.com>
2023-12-05 06:48:11 -0800

    more practice with inheritance

---
+++ JavaProgammingRefresh/src/day46_Inheritance/Class.java
@@ -0,0 +1,10 @@
+package day46_Inheritance;
+
+public class Class {
+
+    private String brand;
+    private String model;
+    private int year;
+    private String color;
+    private double miles;
+}
