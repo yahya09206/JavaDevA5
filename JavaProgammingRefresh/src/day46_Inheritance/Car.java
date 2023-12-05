c8036baf4eabd5cb384a176bfd2a2d3675c9c6a5
Yahya <yahyahussein91@gmail.com>
2023-12-05 06:53:55 -0800

    re added constructor

--- JavaProgammingRefresh/src/day46_Inheritance/Car.java
+++ JavaProgammingRefresh/src/day46_Inheritance/Car.java
@@ -9,6 +9,14 @@ public class Car {
     public long miles;
 
 
+    public Car(String brand, String model, int year, double price, String color, long miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
 
     public void start(){
 
