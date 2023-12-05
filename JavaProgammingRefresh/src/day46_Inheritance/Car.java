050fba66bec88234f35b1906ee91dc1c6e298d87
Yahya <yahyahussein91@gmail.com>
2023-12-05 06:49:32 -0800

    added getters and setters

--- JavaProgammingRefresh/src/day46_Inheritance/Car.java
+++ JavaProgammingRefresh/src/day46_Inheritance/Car.java
@@ -16,5 +16,43 @@ public class Car {
         this.miles = miles;
     }
 
+    public String getBrand() {
+        return brand;
+    }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getMiles() {
+        return miles;
+    }
+
+    public void setMiles(double miles) {
+        this.miles = miles;
+    }
 }
