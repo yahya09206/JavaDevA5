4a19acdc4c6dfda292644ca9d4e66907b44230b1
Yahya <yahyahussein91@gmail.com>
2023-12-05 07:21:34 -0800

    done with inheritance

--- JavaProgammingRefresh/src/day46_Inheritance/carTask/BMW.java
+++ JavaProgammingRefresh/src/day46_Inheritance/carTask/BMW.java
@@ -2,6 +2,8 @@ package day46_Inheritance.carTask;
 
 public class BMW extends Car{
 
+    public static boolean isLuxuryBrand = true;
+
     public BMW(String model, int year, double price, String color, long miles) {
         super("BMW", model, year, price, color, miles);
     }
