517d4f06545505afde30e693b800c6cb30f2d116
Yahya <yahyahussein91@gmail.com>
2023-12-05 06:52:54 -0800

    messed up on variables

--- JavaProgammingRefresh/src/day46_Inheritance/Car.java
+++ JavaProgammingRefresh/src/day46_Inheritance/Car.java
@@ -2,59 +2,13 @@ package day46_Inheritance;
 
 public class Car {
 
-    private String brand;
-    private String model;
-    private int year;
-    private String color;
-    private double miles;
+    public String brand, model;
+    public final int year;
+    public double price;
+    public String color;
+    public long miles;
 
-    public Car(String brand, String model, int year, String color, double miles) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.color = color;
-        this.miles = miles;
-    }
 
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public double getMiles() {
-        return miles;
-    }
-
-    public void setMiles(double miles) {
-        this.miles = miles;
-    }
 
     public void start(){
 
