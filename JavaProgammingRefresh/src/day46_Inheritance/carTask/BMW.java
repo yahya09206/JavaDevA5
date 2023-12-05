8e07503beab1806d6ea3d098c1b30291280fbaa4
Yahya <yahyahussein91@gmail.com>
2023-12-05 07:06:30 -0800

    added new bmw class

---
+++ JavaProgammingRefresh/src/day46_Inheritance/carTask/BMW.java
@@ -0,0 +1,8 @@
+package day46_Inheritance.carTask;
+
+public class BMW extends Car{
+
+    public BMW(String brand, String model, int year, double price, String color, long miles) {
+        super(brand, model, year, price, color, miles);
+    }
+}
