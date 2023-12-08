89a110c7302a601cbd7baab204a918298de5d51e
Yahya <yahyahussein91@gmail.com>
2023-12-07 20:34:15 -0800

    crunch time

--- JavaProgammingRefresh/src/day46_Inheritance/carTask/Car.java
+++ JavaProgammingRefresh/src/day46_Inheritance/carTask/Car.java
@@ -19,7 +19,7 @@ public class Car {
         this.miles = miles;
     }
 
-
+    // instance mmethod
     public void start(){
         System.out.println(brand + " " + model + " is starting");
     }
