4906b611008f159029e485a0e70bb9cecc374620
Yahya <yahyahussein91@gmail.com>
2023-12-11 18:18:36 -0800

    starting java shorts soon

--- JavaProgammingRefresh/src/day46_Inheritance/carTask/Toyota.java
+++ JavaProgammingRefresh/src/day46_Inheritance/carTask/Toyota.java
@@ -4,6 +4,7 @@ public class Toyota extends Car{
 
     public static boolean isReliable = true;
 
+    // starting java shorts soon
     public Toyota(String model, int year, double price, String color, long miles) {
         super("Toyota", model, year, price, color, miles);
     }
