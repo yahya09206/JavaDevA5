aa76189524826fac34488682eaa2553638c3838a
Yahya <yahyahussein91@gmail.com>
2023-12-14 14:41:26 -0800

    setting up person class

--- JavaProgammingRefresh/src/day48_recap/cydeoTask/Person.java
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Person.java
@@ -1,4 +1,11 @@
 package day48_recap.cydeoTask;
 
+import java.time.LocalDate;
+
 public class Person {
+
+    private String name;
+    private char gender;
+    private int age;
+    private LocalDate dOB;
 }
