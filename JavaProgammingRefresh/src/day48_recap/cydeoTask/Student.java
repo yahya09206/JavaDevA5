ab902e608899a8b98fe830b08cac64920146540d
Yahya <yahyahussein91@gmail.com>
2023-12-14 16:15:46 -0800

    set up getters and setters for student class

--- JavaProgammingRefresh/src/day48_recap/cydeoTask/Student.java
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Student.java
@@ -9,7 +9,23 @@ public class Student extends Person{
 
     public Student(String name, char gender, LocalDate dOB, String schoolName, int id) {
         super(name, gender, dOB);
+        setSchoolName(schoolName);
+        setId(id);
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
         this.schoolName = schoolName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
         this.id = id;
     }
 }
