7dad61452271d5cf2e19f622a9fe4d88081f8e18
Yahya <yahyahussein91@gmail.com>
2023-12-14 16:03:18 -0800

    generate constructor

--- JavaProgammingRefresh/src/day48_recap/cydeoTask/Employee.java
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Employee.java
@@ -1,6 +1,17 @@
 package day48_recap.cydeoTask;
 
+import java.time.LocalDate;
+
 public class Employee extends Person{
 
+    private int employeeId;
+    private String jobTitle;
+    private double salary;
 
+    public Employee(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary) {
+        super(name, gender, dOB);
+        this.employeeId = employeeId;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
 }
