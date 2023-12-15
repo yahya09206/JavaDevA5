04692f90b23d3255f67d6fa8d8d1c274bf737395
Yahya <yahyahussein91@gmail.com>
2023-12-14 16:28:05 -0800

    added tester sub class to employee

---
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Tester.java
@@ -0,0 +1,10 @@
+package day48_recap.cydeoTask;
+
+import java.time.LocalDate;
+
+public class Tester extends Employee{
+
+    public Tester(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary) {
+        super(name, gender, dOB, employeeId, jobTitle, salary);
+    }
+}
