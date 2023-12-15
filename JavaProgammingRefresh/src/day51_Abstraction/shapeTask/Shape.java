6884b5491ad6d56a9a824c8a08d84932db902da7
Yahya <yahyahussein91@gmail.com>
2023-12-15 15:14:01 -0800

    setting up class for abstraction

---
+++ JavaProgammingRefresh/src/day51_Abstraction/shapeTask/Shape.java
@@ -0,0 +1,18 @@
+package day51_Abstraction.shapeTask;
+
+public class Shape {
+
+    private String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
