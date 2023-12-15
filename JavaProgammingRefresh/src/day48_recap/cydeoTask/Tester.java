713c15b354659dc924fc3ea1e531b777c61ca4e6
Yahya <yahyahussein91@gmail.com>
2023-12-14 16:29:51 -0800

    added overriden methods

--- JavaProgammingRefresh/src/day48_recap/cydeoTask/Tester.java
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Tester.java
@@ -7,4 +7,14 @@ public class Tester extends Employee{
     public Tester(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary) {
         super(name, gender, dOB, employeeId, jobTitle, salary);
     }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle() + " " + getName() + " is testing");
+    }
+
+    @Override
+    public void attendMeeting() {
+        System.out.println(getJobTitle() + " " + getName() + " is in a meeting");
+    }
 }
