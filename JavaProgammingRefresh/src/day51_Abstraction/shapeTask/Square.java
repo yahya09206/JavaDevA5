23a97fb8efd82de4d2c2833ae86ae6b735cafd1a
Yahya <yahyahussein91@gmail.com>
2023-12-18 15:22:24 -0800

    setting up square class

---
+++ JavaProgammingRefresh/src/day51_Abstraction/shapeTask/Square.java
@@ -0,0 +1,14 @@
+package day51_Abstraction.shapeTask;
+
+public class Square {
+
+    private double s;
+
+    public double getS() {
+        return s;
+    }
+
+    public void setS(double s) {
+        this.s = s;
+    }
+}
