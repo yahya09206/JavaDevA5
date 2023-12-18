eb5c0d63b48bdb4cc4da7017c687e96aaa9f002f
Yahya <yahyahussein91@gmail.com>
2023-12-18 15:07:58 -0800

    added constructor from parent class

--- JavaProgammingRefresh/src/day51_Abstraction/shapeTask/Circle.java
+++ JavaProgammingRefresh/src/day51_Abstraction/shapeTask/Circle.java
@@ -1,9 +1,14 @@
 package day51_Abstraction.shapeTask;
 
-public class Circle {
+public class Circle extends Shape{
 
     public double r;
     public final static double PI = 3.14;
+
+    public Circle(String name, double r) {
+        super(name);
+        this.r = r;
+    }
 
     public double getR() {
         return r;
