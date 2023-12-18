95d5464ca47606524cdc0357b79541fdedc65f3b
Yahya <yahyahussein91@gmail.com>
2023-12-18 15:07:16 -0800

    added getter and setter

--- JavaProgammingRefresh/src/day51_Abstraction/shapeTask/Circle.java
+++ JavaProgammingRefresh/src/day51_Abstraction/shapeTask/Circle.java
@@ -4,4 +4,12 @@ public class Circle {
 
     public double r;
     public final static double PI = 3.14;
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
 }
