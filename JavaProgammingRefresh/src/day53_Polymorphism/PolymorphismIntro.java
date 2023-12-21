dbfea7ca3500d12863c71e362398893dfca103e4
Yahya <yahyahussein91@gmail.com>
2023-12-20 16:04:45 -0800

    using poly to create child objects

--- JavaProgammingRefresh/src/day53_Polymorphism/PolymorphismIntro.java
+++ JavaProgammingRefresh/src/day53_Polymorphism/PolymorphismIntro.java
@@ -1,4 +1,13 @@
 package day53_Polymorphism;
 
+import day51_Abstraction.shapeTask.Circle;
+import day51_Abstraction.shapeTask.Shape;
+import day51_Abstraction.shapeTask.Square;
+
 public class PolymorphismIntro {
+    public static void main(String[] args) {
+
+        Shape circle = new Circle(5);
+        Shape square = new Square(10);
+    }
 }
