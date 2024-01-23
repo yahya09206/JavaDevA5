8c47444d01c082321aec4e93ec2a2f5ad40a10e7
Yahya <yahyahussein91@gmail.com>
2024-01-22 16:27:40 -0800

    program to concat two strings

---
+++ JavaProgammingRefresh/src/day16_string/StringPractice2.java
@@ -0,0 +1,11 @@
+package day16_string;
+
+import java.util.Scanner;
+
+public class StringPractice2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two words");
+    }
+}
