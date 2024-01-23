c231cb1fe94f84064a465cba89a64bb051dbbd86
Yahya <yahyahussein91@gmail.com>
2024-01-22 16:10:16 -0800

    set up scanner

---
+++ JavaProgammingRefresh/src/day16_string/StringPractice1.java
@@ -0,0 +1,16 @@
+package day16_string;
+
+import java.util.Scanner;
+
+public class StringPractice1 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+    }
+}
+/**
+ * Write a method that asks user to enter a string.
+ * if the string is empty, print: string is empty
+ * if the string has more than 3 character, print the last 3 characters
+ * if the string has less than or equal to 3 characters, print the string itself
+ */
