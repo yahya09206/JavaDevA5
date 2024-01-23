bd639537787df01df6c3ba4378b8a1ec850b6c0f
Yahya <yahyahussein91@gmail.com>
2024-01-22 16:35:48 -0800

    completed checks for length of strings

---
+++ JavaProgammingRefresh/src/day16_string/StringPractice3.java
@@ -0,0 +1,26 @@
+package day16_string;
+
+import java.util.Scanner;
+
+public class StringPractice3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word with 3 characters");
+        String word = scanner.next();
+
+        if (word.length() == 3){
+            if (word.charAt(1) == 'a'){
+                System.out.println("cool word");
+            } else {
+                System.out.println("okay word");
+            }
+        } else {
+            if (word.length() < 3){
+                System.out.println("Word is too short");
+            }else {
+                System.out.println("Word is too long");
+            }
+        }
+    }
+}
