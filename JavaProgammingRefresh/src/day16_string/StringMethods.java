04eb2a98cdd2b14c4008a37ecd5466268d4a31bb
Yahya <yahyahussein91@gmail.com>
2024-01-23 17:03:11 -0800

    nothing for today

--- JavaProgammingRefresh/src/day16_string/StringMethods.java
+++ JavaProgammingRefresh/src/day16_string/StringMethods.java
@@ -8,6 +8,7 @@ public class StringMethods {
 
         System.out.println(email);
 
+        // replace method
         String sentence = "C# is fun, C# is cool";
         sentence = sentence.replace("C#", "Java");
         System.out.println("sentence = " + sentence);
