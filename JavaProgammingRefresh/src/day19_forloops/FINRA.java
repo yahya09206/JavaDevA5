4ab0951580f1a54b89e97541f0363cea19fce006
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:34:24 -0800

    setting up for divisible by 3

--- JavaProgammingRefresh/src/day19_forloops/FINRA.java
+++ JavaProgammingRefresh/src/day19_forloops/FINRA.java
@@ -6,6 +6,8 @@ public class FINRA {
         for (int i = 0; i <= 100; i++){
             if (i % 15 == 0){
                 System.out.println("FINRA");
+            } else if (i % 3 == 0){
+                System.out.println("FIN ");
             }
         }
     }
