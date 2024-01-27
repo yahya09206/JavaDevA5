52ad314b44e0c0469ca0f1ec82ac461baf5cb5dc
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:56:14 -0800

    forgot some minor details

--- JavaProgammingRefresh/src/day19_forloops/StringReverse.java
+++ JavaProgammingRefresh/src/day19_forloops/StringReverse.java
@@ -10,6 +10,11 @@ public class StringReverse {
         String name = scanner.next();
         String reversedName = "";
 
+        for (int i = name.length() - 1; i >= 0; i--){
+            reversedName += name.charAt(i);
+        }
+
+        System.out.println("reversedName = " + reversedName);
 
     }
 }
