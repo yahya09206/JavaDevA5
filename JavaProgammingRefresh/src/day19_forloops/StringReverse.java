87f6076f6761b737c39e34be6aa5429dfe82fe54
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:53:29 -0800

    setting up for reversing a string

---
+++ JavaProgammingRefresh/src/day19_forloops/StringReverse.java
@@ -0,0 +1,15 @@
+package day19_forloops;
+
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name");
+        String name = scanner.next();
+        String reversedName = "";
+
+
+    }
+}
