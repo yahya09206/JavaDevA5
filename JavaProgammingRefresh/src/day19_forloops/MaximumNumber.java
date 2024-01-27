fae885a7be20c558681aa1ad5117ef15c13a59b1
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:47:05 -0800

    assigning larger number to max variable

--- JavaProgammingRefresh/src/day19_forloops/MaximumNumber.java
+++ JavaProgammingRefresh/src/day19_forloops/MaximumNumber.java
@@ -8,5 +8,15 @@ public class MaximumNumber {
 
         Scanner scanner = new Scanner(System.in);
         int max = -2147483648;
+
+        for (int i = 0; i <= 5; i++){
+            int num = scanner.nextInt();
+
+            if (num > max){
+                max = num;
+            }
+        }
+
+        System.out.println("max = " + max);
     }
 }
