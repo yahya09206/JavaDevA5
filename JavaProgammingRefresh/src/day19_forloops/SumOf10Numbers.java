34f5b5e41df63ac4e59e1e63f90403d0b05fd470
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:40:28 -0800

    adding number to result

--- JavaProgammingRefresh/src/day19_forloops/SumOf10Numbers.java
+++ JavaProgammingRefresh/src/day19_forloops/SumOf10Numbers.java
@@ -7,10 +7,16 @@ public class SumOf10Numbers {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 10 numbers");
+        int result = 0;
 
         for (int i = 0; i <= 10; i++){
             System.out.println("Enter number: " + i);
             int number = scanner.nextInt();
+            result += number;
         }
+
+        System.out.println("result = " + result);
+
+
     }
 }
