18cb9b83fa83ff2e124483f8ec8f1221f3b794c0
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:33:55 -0800

    setting up for divisible by 15

--- JavaProgammingRefresh/src/day19_forloops/FINRA.java
+++ JavaProgammingRefresh/src/day19_forloops/FINRA.java
@@ -3,5 +3,10 @@ package day19_forloops;
 public class FINRA {
     public static void main(String[] args) {
 
+        for (int i = 0; i <= 100; i++){
+            if (i % 15 == 0){
+                System.out.println("FINRA");
+            }
+        }
     }
 }
