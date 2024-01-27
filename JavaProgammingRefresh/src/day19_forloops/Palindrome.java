5bdb21aa0269fec38ce0a8a8f26d9c49e76eeb43
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:57:50 -0800

    setting up for palindrome

--- JavaProgammingRefresh/src/day19_forloops/Palindrome.java
+++ JavaProgammingRefresh/src/day19_forloops/Palindrome.java
@@ -1,20 +1,7 @@
 package day19_forloops;
 
-import java.util.Scanner;
-
 public class Palindrome {
     public static void main(String[] args) {
 
-        System.out.println("Enter a word");
-        String word = new Scanner(System.in).next();
-        String reversedWord = "";
-
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversedWord += word.charAt(i);
-        }
-
-        boolean isPalindrome = word.equals(reversedWord);
-
-        System.out.println("isPalindrome = " + isPalindrome);
     }
 }
