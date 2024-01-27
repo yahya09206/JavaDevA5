66eb97c679145dae587600478f7e3b8065039129
Yahya <yahyahussein91@gmail.com>
2024-01-27 15:45:29 -0800

    assigned max int to lowest minimal number

--- JavaProgammingRefresh/src/day19_forloops/MaximumNumber.java
+++ JavaProgammingRefresh/src/day19_forloops/MaximumNumber.java
@@ -1,8 +1,12 @@
 package day19_forloops;
 
+import java.util.Scanner;
+
 public class MaximumNumber {
     public static void main(String[] args) {
 
 
+        Scanner scanner = new Scanner(System.in);
+        int max = -2147483648;
     }
 }
