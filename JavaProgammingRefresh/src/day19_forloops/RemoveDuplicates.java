784583055b936106e9f6b54159b71ac90576f928
Yahya <yahyahussein91@gmail.com>
2024-01-27 16:04:55 -0800

    removing duplicates

--- JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
+++ JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
@@ -3,16 +3,6 @@ package day19_forloops;
 public class RemoveDuplicates {
     public static void main(String[] args) {
 
-        String word = "ababababab";
-        String result = "";
 
-        for (int i = 0; i <= word.length() - 1; i++) {
-
-            if (!result.contains("" + word.charAt(i))){
-                result += word.charAt(i);
-            }
-        }
-
-        System.out.println("result = " + result);
     }
 }
