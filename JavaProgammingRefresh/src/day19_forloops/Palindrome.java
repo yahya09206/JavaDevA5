f718947cc43b80eeb3b73dd66a55c0d4f1ce2499
Yahya <yahyahussein91@gmail.com>
2024-01-27 16:00:39 -0800

    forgot to equal

--- JavaProgammingRefresh/src/day19_forloops/Palindrome.java
+++ JavaProgammingRefresh/src/day19_forloops/Palindrome.java
@@ -1,7 +1,19 @@
 package day19_forloops;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
 
+        String word = new Scanner(System.in).next();
+        String reversedWord = "";
+
+        for (int i = word.length() - 1; i >= 0; i--){
+            reversedWord += word.charAt(i);
+        }
+
+        boolean isPalindrome = reversedWord.equals(word);
+
+        System.out.println("isPalindrome = " + isPalindrome);
     }
 }
