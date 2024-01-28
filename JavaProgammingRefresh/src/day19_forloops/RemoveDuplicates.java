5387fb936e2a7522edf72e2ab5ea42c36976ce54
Yahya <yahyahussein91@gmail.com>
2024-01-27 16:07:47 -0800

    checking if character is contained in result

--- JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
+++ JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
@@ -4,5 +4,14 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
 
 
+        String word = "ababababab";
+        String result = "";
+
+        for (int i = 0; i <= word.length() - 1; i++){
+
+            if (!result.contains("" + word.charAt(i))){
+                result += word.charAt(i);
+            }
+        }
     }
 }
