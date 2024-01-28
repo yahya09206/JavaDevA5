9abb0b2f0feed5e030b354223cfc39b7ddd125ed
Yahya <yahyahussein91@gmail.com>
2024-01-27 16:01:50 -0800

    added prompt

--- JavaProgammingRefresh/src/day19_forloops/Palindrome.java
+++ JavaProgammingRefresh/src/day19_forloops/Palindrome.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
 
-        String word = new Scanner(System.in).next();
         System.out.println("Enter a word");
+        String word = new Scanner(System.in).next();
         String reversedWord = "";
 
         for (int i = word.length() - 1; i >= 0; i--){
