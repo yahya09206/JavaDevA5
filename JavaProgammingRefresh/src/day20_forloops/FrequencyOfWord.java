103a1355b6b97e33eaf3dd74b91107c222e5beab
Yahya <yahyahussein91@gmail.com>
2024-01-27 16:25:43 -0800

    added comments

---
+++ JavaProgammingRefresh/src/day20_forloops/FrequencyOfWord.java
@@ -0,0 +1,20 @@
+package day20_forloops;
+
+public class FrequencyOfWord {
+    public static void main(String[] args) {
+
+        String sentence = "Java Java";
+        int count = 0;
+
+        // loop through word ending with length of word
+        for (int i = 0; i <= sentence.length() - 4; i++){
+            // start with beginning of word until the end of its length
+            String each = sentence.substring(i, i + 4);
+            if (each.equals("Java")){
+                count++;
+            }
+        }
+
+        System.out.println("count = " + count);
+    }
+}
