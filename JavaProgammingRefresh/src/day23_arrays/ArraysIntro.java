b643ba29926fa63210b218dedf6399136c3c3f3f
Yahya <yahyahussein91@gmail.com>
2024-02-10 23:23:07 -0800

    assignment due tomorrow

--- JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
+++ JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
@@ -3,7 +3,8 @@ package day23_arrays;
 public class ArraysIntro {
     public static void main(String[] args) {
 
-        // add students to array
+        // add students to array using brack notation
+
 
     }
 }
