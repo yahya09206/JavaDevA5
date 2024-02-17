95c1b56f1bd15ef4cebe009042928b4b24e966f8
Yahya <yahyahussein91@gmail.com>
2024-02-17 10:54:08 -0800

    shapes with loops

---
+++ JavaProgammingRefresh/src/day19_forloops/Shape.java
@@ -0,0 +1,10 @@
+package day19_forloops;
+
+public class Shape {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 8; i++){
+            System.out.println("* * * * * *");
+        }
+    }
+}
---
+++ JavaProgammingRefresh/src/day19_forloops/Shape2.java
@@ -0,0 +1,12 @@
+package day19_forloops;
+
+public class Shape2 {
+    public static void main(String[] args) {
+
+        System.out.println("\t\t* * * * * * * *");
+        for (int i = 1; i <= 6; i++){
+            System.out.println("\t\t*             *");
+        }
+        System.out.println("\t\t* * * * * * * *");
+    }
+}
