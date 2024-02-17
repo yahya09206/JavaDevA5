6dc10ab728418b547aed834bb5123a8df991bad9
Yahya <yahyahussein91@gmail.com>
2024-02-17 10:40:31 -0800

    print from Z to A

--- JavaProgammingRefresh/src/day19_forloops/ForLoopPractice2.java
+++ JavaProgammingRefresh/src/day19_forloops/ForLoopPractice2.java
@@ -6,5 +6,12 @@ public class ForLoopPractice2 {
         for (char i = 'A'; i <= 'Z'; i++){
             System.out.print(i + " ");
         }
+
+        System.out.println();
+        System.out.println("----------------------------------");
+
+        for (char i = 'Z'; i >= 'A'; i--){
+            System.out.print(i + " ");
+        }
     }
 }
