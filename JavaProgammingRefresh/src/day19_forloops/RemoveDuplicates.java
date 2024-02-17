7650bfb109ed35dcec328ef5a2920107f2209637
Yahya <yahyahussein91@gmail.com>
2024-02-17 11:22:11 -0800

    trying with different words

--- JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
+++ JavaProgammingRefresh/src/day19_forloops/RemoveDuplicates.java
@@ -3,14 +3,17 @@ package day19_forloops;
 public class RemoveDuplicates {
     public static void main(String[] args) {
 
-        String word = "abababababa";
+        String word = "churro";
         String result = "";
 
-        for (int i = 0; i <= word.length(); i++){
+        for (int i = 0; i <= word.length() - 1; i++){
             // check to see if result contains character
             if (!result.contains(""+word.charAt(i))){
+                // add character to result variable
                 result += word.charAt(i);
             }
         }
+
+        System.out.println("result = " + result);
     }
 }
