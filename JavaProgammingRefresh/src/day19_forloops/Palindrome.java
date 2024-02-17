16182bacf3464a16b7d1fd6b98c4c4185e244986
Yahya <yahyahussein91@gmail.com>
2024-02-17 11:13:56 -0800

    checking for palindrome from user input

--- JavaProgammingRefresh/src/day19_forloops/Palindrome.java
+++ JavaProgammingRefresh/src/day19_forloops/Palindrome.java
@@ -1,8 +1,25 @@
 package day19_forloops;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String name = scanner.next();
+        String reversedName = "";
+        boolean isPalindrome = false;
 
+        for (int i = name.length() - 1; i >= 0; i--){
+            char each = name.charAt(i);
+            reversedName += each;
+
+            if (reversedName.equals(name)){
+                isPalindrome = true;
+            }
+        }
+
+        System.out.println(isPalindrome);
     }
 }
