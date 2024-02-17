83571dd75c4d10bcd76285b25ed2f168a21cc878
Yahya <yahyahussein91@gmail.com>
2024-02-17 10:38:48 -0800

    print A to Z

--- JavaProgammingRefresh/src/day19_forloops/ForLoopPractice.java
+++ JavaProgammingRefresh/src/day19_forloops/ForLoopPractice.java
@@ -6,5 +6,30 @@ public class ForLoopPractice {
         for (int i = 0; i <= 1000; i+=25){
             System.out.print(i);
         }
+
+        System.out.println();
+
+        System.out.println("-------------------------------");
+        for (int i = 0; i <= 100; i++){
+            if (i % 2 != 0){
+                System.out.print(i + " ");
+            }
+        }
+
+        System.out.println();
+
+        System.out.println("-------------------------------");
+        for (int i = 1; i <= 100; i+=2){
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("-------------------------------");
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+        }
     }
 }
---
+++ JavaProgammingRefresh/src/day19_forloops/ForLoopPractice2.java
@@ -0,0 +1,10 @@
+package day19_forloops;
+
+public class ForLoopPractice2 {
+    public static void main(String[] args) {
+
+        for (char i = 'A'; i <= 'Z'; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
