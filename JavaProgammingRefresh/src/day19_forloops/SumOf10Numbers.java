c5c4b7d27df971ceb9c6193928a811877fafda1d
Yahya <yahyahussein91@gmail.com>
2024-02-17 10:58:32 -0800

    find total sum of 10 numbers

--- JavaProgammingRefresh/src/day19_forloops/SumOf10Numbers.java
+++ JavaProgammingRefresh/src/day19_forloops/SumOf10Numbers.java
@@ -1,7 +1,30 @@
 package day19_forloops;
 
+import java.util.Scanner;
+
 public class SumOf10Numbers {
     public static void main(String[] args) {
 
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+        System.out.println();
+        System.out.println("-----------------------");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+        int sum2 = 0;
+
+
+        for (int i = 1; i <= num; i++){
+            sum2 += i;
+        }
+
+        System.out.println("sum2 = " + sum2);
     }
 }
