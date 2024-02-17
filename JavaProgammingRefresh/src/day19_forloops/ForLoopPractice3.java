ba3685dc3286f9adddbf7b004454a49e00b3f390
Yahya <yahyahussein91@gmail.com>
2024-02-17 10:49:30 -0800

    fizz buzz remixed

---
+++ JavaProgammingRefresh/src/day19_forloops/ForLoopPractice3.java
@@ -0,0 +1,10 @@
+package day19_forloops;
+
+public class ForLoopPractice3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++){
+            System.out.println("Push up " + i);
+        }
+    }
+}
