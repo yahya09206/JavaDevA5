c66b2beba6689afa5dbd9fb067aeb64cae90fb6c
Yahya <yahyahussein91@gmail.com>
2024-02-17 11:29:01 -0800

    finding unique characters

--- JavaProgammingRefresh/src/day19_forloops/UniqueCharacters.java
+++ JavaProgammingRefresh/src/day19_forloops/UniqueCharacters.java
@@ -3,5 +3,22 @@ package day19_forloops;
 public class UniqueCharacters {
     public static void main(String[] args) {
 
+        String word = "aadddwddffffbcc";
+        // Store unique character inside result
+        String result = "";
+
+        for (int i = 0; i <= word.length() - 1; i++){
+            // split into chars
+            char ch = word.charAt(i);
+            // check if boolean will return true or false based on the first occurrence and its last occurrence
+            boolean isUnique = word.indexOf(ch) == word.lastIndexOf(ch);
+            // check if char only appears one time
+            if (isUnique){
+                // add unique char to result variable
+                result += ch;
+            }
+        }
+
+        System.out.println("result = " + result);
     }
 }
