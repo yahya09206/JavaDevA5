5804ebfd86fcd37f6e18766990e4b618b8fb9576
Yahya <yahyahussein91@gmail.com>
2024-02-17 11:11:47 -0800

    fixed to generalize

--- JavaProgammingRefresh/src/day19_forloops/StringReverse.java
+++ JavaProgammingRefresh/src/day19_forloops/StringReverse.java
@@ -1,9 +1,13 @@
 package day19_forloops;
 
+import java.util.Scanner;
+
 public class StringReverse {
     public static void main(String[] args) {
 
-        String name = "Mikey";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name");
+        String name = scanner.next();
         String reversedName = "";
 
         for (int i = name.length() - 1; i >= 0; i--){
