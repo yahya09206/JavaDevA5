ce98ad5d8d627a73658b91bd0659e7c629719144
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:17:21 -0800

    printing student and their grade

--- JavaProgammingRefresh/src/day23_arrays/HighestAndLowestScores.java
+++ JavaProgammingRefresh/src/day23_arrays/HighestAndLowestScores.java
@@ -5,5 +5,9 @@ public class HighestAndLowestScores {
 
         int[] numbers = {85, 70, 95, 90, 100};
         String[] names = {"Mike", "Adam", "Tonny", "John", "Amy"};
+
+        for (int i = 0; i <= numbers.length - 1; i++){
+            System.out.println(names[i] + " Grade: " + numbers[i]);
+        }
     }
 }
