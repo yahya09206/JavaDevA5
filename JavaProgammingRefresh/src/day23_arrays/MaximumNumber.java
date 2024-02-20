2d83023ac6ab5424f342b6bab45077650afdab3f
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:06:03 -0800

    finding max number from array

--- JavaProgammingRefresh/src/day23_arrays/MaximumNumber.java
+++ JavaProgammingRefresh/src/day23_arrays/MaximumNumber.java
@@ -3,6 +3,7 @@ package day23_arrays;
 public class MaximumNumber {
     public static void main(String[] args) {
 
-
+        int maxNumber = {1, 4, 33, 2, 45};
+        int max = -2147483648;
     }
 }
