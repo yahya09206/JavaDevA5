a5466d4a99bfcb6fdad4ede005a57c81dfcde52c
Yahya <yahyahussein91@gmail.com>
2024-02-19 16:47:01 -0800

    array basics

--- JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
+++ JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
@@ -3,7 +3,15 @@ package day23_arrays;
 public class ArraysIntro {
     public static void main(String[] args) {
 
-        // add students to array using bracket notation
+        // Declare array
+        String[] group10 = {"James", "Maria", "Mikey", "Anthony", "Annie"};
+        // Retrieve
+        System.out.println(group10[2]);
+
+        // Loop thru and print out names
+        for (int i = 0; i <= group10.length - 1; i++){
+            System.out.println(group10[i]);
+        }
 
 
     }
