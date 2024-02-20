3225baab4fa061cd9075747ea014d8f28e57c5d3
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:11:28 -0800

    finding max and min of array

--- JavaProgammingRefresh/src/day23_arrays/MaximumNumber.java
+++ JavaProgammingRefresh/src/day23_arrays/MaximumNumber.java
@@ -3,12 +3,21 @@ package day23_arrays;
 public class MaximumNumber {
     public static void main(String[] args) {
 
-        int[] maxNumber = {1, 4, 33, 2, 45};
+        int[] maxNumber = {1, -4, 33, 2, 45};
         int max = -2147483648;
         int min = 2147483647;
 
         for (int i = 0; i <= maxNumber.length - 1; i++){
+            if (maxNumber[i] > max){
+                max = maxNumber[i];
+            }
 
+            if (maxNumber[i] < min){
+                min = maxNumber[i];
+            }
         }
+
+        System.out.println(max);
+        System.out.println(min);
     }
 }
