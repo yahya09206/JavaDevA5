a44ea092c9599f2de9c59ef92206801f6a9b699a
Yahya <yahyahussein91@gmail.com>
2024-02-19 17:08:33 -0800

    declaring arrays

--- JavaProgammingRefresh/src/day23_arrays/Months.java
+++ JavaProgammingRefresh/src/day23_arrays/Months.java
@@ -1,8 +1,18 @@
 package day23_arrays;
 
+import java.util.Scanner;
+
 public class Months {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        String[] months = {"Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        System.out.print("Please select a Month: ");
+        int month = scanner.nextInt();
+
+        if (month == months[0]){
+
+        }
 
     }
 }
