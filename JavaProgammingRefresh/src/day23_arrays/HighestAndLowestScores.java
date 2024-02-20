21e48aead8553bd5aa2b13f91222a51859b89a33
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:21:54 -0800

    printing out highest score with their name

--- JavaProgammingRefresh/src/day23_arrays/HighestAndLowestScores.java
+++ JavaProgammingRefresh/src/day23_arrays/HighestAndLowestScores.java
@@ -3,11 +3,25 @@ package day23_arrays;
 public class HighestAndLowestScores {
     public static void main(String[] args) {
 
-        int[] numbers = {85, 70, 95, 90, 100};
+        int[] scores = {85, 70, 95, 90, 100};
         String[] names = {"Mike", "Adam", "Tonny", "John", "Amy"};
 
-        for (int i = 0; i <= numbers.length - 1; i++){
-            System.out.println(names[i] + " Grade: " + numbers[i]);
+        // variables to store max score and name with the highest score
+        int maxScore = scores[0];
+        String maxName = "";
+
+        for (int i = 0; i <= scores.length - 1; i++){
+            // Variables to represent names and scores
+            String eachName = names[i];
+            int eachScore = scores[i];
+
+            if (eachScore > maxScore){
+                maxScore = eachScore;
+                maxName = eachName;
+            }
         }
+
+        System.out.println(maxScore);
+        System.out.println(maxName);
     }
 }
