7d0b54d670db27873f7bb756f1295eb9d69ad5b3
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:04:01 -0800

    replacing null values with actual values

--- JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
+++ JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
@@ -9,5 +9,11 @@ public class MyClassMates {
         for (int i = 0; i <= classmates.length - 1; i++){
             System.out.println(classmates[i]);
         }
+
+        classmates[0] = "Jamal";
+        classmates[1] = "Jaime";
+        classmates[2] = "Jessica";
+        classmates[3] = "Wendy";
+        classmates[4] = "Akbar";
     }
 }
