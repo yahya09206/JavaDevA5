d002c839b2c819c5c3dc11a7de4b04ce8cc2d290
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:02:06 -0800

    printing null array

--- JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
+++ JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
@@ -3,6 +3,11 @@ package day23_arrays;
 public class MyClassMates {
     public static void main(String[] args) {
 
-        String[] classmates;
+        // declares like this for when you know how many items
+        String[] classmates = new String[5];
+
+        for (int i = 0; i <= classmates.length - 1; i++){
+            System.out.println(classmates[i]);
+        }
     }
 }
