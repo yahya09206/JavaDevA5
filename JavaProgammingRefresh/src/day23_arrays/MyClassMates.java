929d8a5e0811ad1305a48a71d5b6e689d291a58c
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:04:57 -0800

    printing out as expected

--- JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
+++ JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
@@ -5,15 +5,14 @@ public class MyClassMates {
 
         // declares like this for when you know how many items
         String[] classmates = new String[5];
-
-        for (int i = 0; i <= classmates.length - 1; i++){
-            System.out.println(classmates[i]);
-        }
-
         classmates[0] = "Jamal";
         classmates[1] = "Jaime";
         classmates[2] = "Jessica";
         classmates[3] = "Wendy";
         classmates[4] = "Akbar";
+
+        for (int i = 0; i <= classmates.length - 1; i++){
+            System.out.println(classmates[i]);
+        }
     }
 }
