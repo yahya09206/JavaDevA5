c00d87e0a9e6ce46f0e1dff6823393f1a9bf9042
Yahya <yahyahussein91@gmail.com>
2024-02-20 14:29:06 -0800

    iterating thru user input

--- JavaProgammingRefresh/src/day23_arrays/AverageNumber.java
+++ JavaProgammingRefresh/src/day23_arrays/AverageNumber.java
@@ -7,6 +7,18 @@ public class AverageNumber {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many numbers do you want to enter");
+        // assign size of array to be what the user enters
         int[] nums = new int[scanner.nextInt()];
+        // variable to count
+        int sum = 0;
+
+        for (int i = 0; i <= nums.length - 1; i++){
+            // Prompt user for input
+            System.out.println("Enter a number");
+            // go through index for each number
+            nums[i] = scanner.nextInt();
+            // add numbers to sum variable
+            sum += nums[i];
+        }
     }
 }
