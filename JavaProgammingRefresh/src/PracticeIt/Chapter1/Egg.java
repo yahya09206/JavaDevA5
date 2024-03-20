07963687adfbf2b46c1b74a525a4d6a8d397cfd4
Yahya <yahyahussein91@gmail.com>
2024-03-20 10:34:34 -0700

    completed egg task

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/Egg.java
@@ -0,0 +1,24 @@
+package PracticeIt.Chapter1;
+
+public class Egg {
+    public static void main(String[] args) {
+        top();
+        middle();
+        bottom();
+    }
+
+    public static void top(){
+        System.out.println("   _______");
+        System.out.println("  /       \\");
+        System.out.println(" /         \\");
+    }
+
+    public static void middle(){
+        System.out.println(" _\"_'_\"_'_\"_");
+    }
+
+    public static void bottom(){
+        System.out.println(" \\         /");
+        System.out.println("  \\_______/");
+    }
+}
