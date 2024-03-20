db607955e237fd1f7ee59af20c023fe2f34f23c8
Yahya <yahyahussein91@gmail.com>
2024-03-20 09:32:30 -0700

    completed well formed task

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/WellFormed.java
@@ -0,0 +1,12 @@
+package PracticeIt.Chapter1;
+
+public class WellFormed {
+    public static void main(String[] args) {
+
+        System.out.println("A well-formed Java program has \na main method with { and } \nbraces.");
+        System.out.println();
+        System.out.println("A System.out.println statement \nhas ( and ) and usually a ");
+        System.out.println("String that starts and ends \nwith a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+    }
+}
