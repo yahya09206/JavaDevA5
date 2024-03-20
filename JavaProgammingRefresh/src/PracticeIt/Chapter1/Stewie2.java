eedc7e8d1041577efe98c77e4032654eb2426296
Yahya <yahyahussein91@gmail.com>
2024-03-20 10:10:53 -0700

    completed stewie2 task

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/Stewie2.java
@@ -0,0 +1,23 @@
+package PracticeIt.Chapter1;
+
+public class Stewie2 {
+    public static void main(String[] args) {
+
+        top();
+        bottom();
+        bottom();
+        bottom();
+        bottom();
+        bottom();
+
+    }
+
+    public static void top(){
+        System.out.println("//////////////////////");
+    }
+
+    public static void bottom(){
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
