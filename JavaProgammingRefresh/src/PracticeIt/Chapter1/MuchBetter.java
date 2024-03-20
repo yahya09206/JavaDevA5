2190b5b54c03cfe6e26a36e95a1a6f72e44de2fd
Yahya <yahyahussein91@gmail.com>
2024-03-20 09:52:43 -0700

    completed much better task

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/MuchBetter.java
@@ -0,0 +1,10 @@
+package PracticeIt.Chapter1;
+
+public class MuchBetter {
+    public static void main(String[] args) {
+
+        System.out.println("A \"quoted\" String is \n'much' better if you learn \nthe rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty String. \nDon't forget: use \\\" instead of \" !");
+        System.out.println("'' is not the same as \"");
+    }
+}
