fdf064f9fd896a2e25da3cdc443811f312abb88d
Yahya <yahyahussein91@gmail.com>
2024-03-21 08:55:46 -0700

    completed fightsong task

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/FightSong.java
@@ -0,0 +1,26 @@
+package PracticeIt.Chapter1;
+
+public class FightSong {
+    public static void main(String[] args) {
+
+        go();
+        System.out.println();
+        go();
+        you();
+        go();
+        System.out.println();
+        go();
+        you();
+        go();
+        System.out.println();
+        go();
+    }
+
+    public static void go(){
+        System.out.println("Go, team, go! \nYou can do it.");
+    }
+
+    public static void you(){
+        System.out.println("You're the best, \nIn the West.");
+    }
+}
