3c82f6ec9aa6eb289df290a34866fdd2a8e4c764
Yahya <yahyahussein91@gmail.com>
2024-05-07 16:35:43 -0700

    frequency of words

--- JavaProgammingRefresh/src/day20_forloops/FrequencyOfWord.java
+++ JavaProgammingRefresh/src/day20_forloops/FrequencyOfWord.java
@@ -3,5 +3,20 @@ package day20_forloops;
 public class FrequencyOfWord {
     public static void main(String[] args) {
 
+        String sentence = "Java Java";
+        // variable to store occurrences
+        int count = 0;
+
+        // stop loop at end of the length of the word or you will get stringindexoutofbounds exception
+        for (int i = 0; i <= sentence.length() - 4; i++){
+            // get substring from beginning of word until the end of it
+            String each = sentence.substring(i, i + 4);
+
+            if (each.equals("Java")){
+                count++;
+            }
+        }
+
+        System.out.println("The frequency of the word Java = " + count);
     }
 }
--- JavaProgammingRefresh/src/day20_forloops/FrequencyOfWord2.java
+++ JavaProgammingRefresh/src/day20_forloops/FrequencyOfWord2.java
@@ -3,5 +3,18 @@ package day20_forloops;
 public class FrequencyOfWord2 {
     public static void main(String[] args) {
 
+
+        String word = "Python Python";
+        int count = 0;
+
+        for (int i = 0; i <= word.length() - 6; i++){
+            String each = word.substring(i, i + 6);
+            System.out.println(each);
+            if (each.equals("Python")){
+                count++;
+            }
+        }
+
+        System.out.println("The frequency of the word Python = " + count);
     }
 }
