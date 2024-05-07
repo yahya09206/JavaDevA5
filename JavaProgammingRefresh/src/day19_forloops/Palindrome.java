4ea0dabaf92259353b4c499d1758dbfc6fff413d
Yahya <yahyahussein91@gmail.com>
2024-05-07 13:16:33 -0700

    palindrome

--- JavaProgammingRefresh/src/day19_forloops/Palindrome.java
+++ JavaProgammingRefresh/src/day19_forloops/Palindrome.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a word");
-        String name = scanner.next();
-        String reversedName = "";
-        boolean isPalindrome = false;
+        String word = new Scanner(System.in).next();
+        String reversedWord = "";
 
-        for (int i = name.length() - 1; i >= 0; i--){
-            char each = name.charAt(i);
-            reversedName += each;
-
-            if (reversedName.equals(name)){
-                isPalindrome = true;
-            }
+        for (int i = word.length() - 1; i >= 0; i--){
+            reversedWord += word.charAt(i);
         }
 
-        System.out.println(isPalindrome);
+        if (reversedWord.equals(word)){
+            System.out.println("Word is a palindrome");
+        } else {
+            System.out.println("Word is not a palindrome");
+        }
+
     }
 }
