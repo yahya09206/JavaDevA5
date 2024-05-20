e4e20d1856866d87ce6e10c53cdc6eb9754c1af7
Yahya <yahyahussein91@gmail.com>
2024-05-20 16:56:10 -0700

    added getters and setters and toString

--- JavaProgammingRefresh/src/day40_statics/Address.java
+++ JavaProgammingRefresh/src/day40_statics/Address.java
@@ -8,4 +8,63 @@ public class Address {
     public String stateName;
     public String zipCode;
     public static String countryName;
+
+    public String getBuildingNumber() {
+        return buildingNumber;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public String getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public static String getCountryName() {
+        return countryName;
+    }
+
+    public static void setCountryName(String countryName) {
+        Address.countryName = countryName;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "buildingNumber='" + buildingNumber + '\'' +
+                ", streetNumber='" + streetNumber + '\'' +
+                ", cityName='" + cityName + '\'' +
+                ", stateName='" + stateName + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
 }
