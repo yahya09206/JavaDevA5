864bf690d69e9cb33badd0f41b8b32cd458d10d0
Yahya <yahyahussein91@gmail.com>
2024-06-30 12:54:40 -0700

    methods without params

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/Mantra.java
@@ -0,0 +1,16 @@
+package PracticeIt.Chapter1;
+
+public class Mantra {
+    public static void main(String[] args) {
+
+        mantras();
+        System.out.println();
+        mantras();
+
+    }
+
+    public static void mantras(){
+        System.out.println("There's one thing every coder must understand:");
+        System.out.println("The System.out.println command.");
+    }
+}
