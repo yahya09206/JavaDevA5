f3b6dbb3380e06bff0e6d509c8a119759986b51c
Yahya <yahyahussein91@gmail.com>
2024-06-30 12:57:56 -0700

    stewie v2 with methods

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/Stewie2.java
@@ -0,0 +1,25 @@
+package PracticeIt.Chapter1;
+
+public class Stewie2 {
+    public static void main(String[] args) {
+
+        topPart();
+        bottomPart();
+        bottomPart();
+        bottomPart();
+        bottomPart();
+        bottomPart();
+
+    }
+
+    public static void topPart(){
+
+        System.out.println("//////////////////////");
+    }
+
+    public static void bottomPart(){
+
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
