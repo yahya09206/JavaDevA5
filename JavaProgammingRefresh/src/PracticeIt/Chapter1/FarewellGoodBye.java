c929debd95401bd1ebac35b0d6ad42f56e4b96e3
Yahya <yahyahussein91@gmail.com>
2024-06-30 14:00:13 -0700

    last one

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter1/FarewellGoodBye.java
@@ -0,0 +1,20 @@
+package PracticeIt.Chapter1;
+
+public class FarewellGoodBye {
+    public static void main(String[] args) {
+
+        openingLine();
+        System.out.println("good night! \nIt's time, to go, \nand I'll be out of sight!");
+        System.out.println();
+        openingLine();
+        System.out.println("take care! \nI'll say, goodbye, \nthat's neither here nor there!");
+        System.out.println();
+        openingLine();
+        System.out.println("see you later! \nI hope, you think, \nI'm a lover, not a hater!");
+    }
+
+    public static void openingLine(){
+
+        System.out.println("Farewell, \ngoodbye, \nau revoir,");
+    }
+}
