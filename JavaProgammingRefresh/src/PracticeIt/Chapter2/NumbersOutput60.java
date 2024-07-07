f4d46b9f0e62c939db76b23e633cf9535a9c1f9a
Yahya <yahyahussein91@gmail.com>
2024-07-06 17:53:57 -0700

    outputting sixty chars

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter2/NumbersOutput60.java
@@ -0,0 +1,19 @@
+package PracticeIt.Chapter2;
+
+public class NumbersOutput60 {
+    public static void main(String[] args) {
+
+        // repeat 6 times on same line
+        for (int i = 0; i < 6; i++){
+            // after every 10 spaces print out a |
+            for (int j = 0; j < 9; j++){
+                System.out.print(" ");
+            }
+            System.out.print("|");
+        }
+        System.out.println();
+        for (int i = 1; i <= 60; i++){
+            System.out.print(i%10);
+        }
+    }
+}
