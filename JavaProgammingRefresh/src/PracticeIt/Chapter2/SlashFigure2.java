f4bbfa9d2e63e09b617fdac7300e48ab55f9fcab
Yahya <yahyahussein91@gmail.com>
2024-07-08 23:30:29 -0700

    window program with constant

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter2/SlashFigure2.java
@@ -0,0 +1,8 @@
+package PracticeIt.Chapter2;
+
+public class SlashFigure2 {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter2/Window.java
@@ -0,0 +1,46 @@
+package PracticeIt.Chapter2;
+
+public class Window {
+    public static final int SIZE = 5;
+    public static void main(String[] args) {
+
+        printLine();
+        System.out.println();
+        printWindow();
+        printLine();
+        System.out.println();
+        printWindow();
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.print("+");
+
+        for(int i = 0; i < SIZE; i++)
+            System.out.print("=");
+
+        System.out.print("+");
+
+        for(int i = 0; i < SIZE; i++)
+            System.out.print("=");
+
+        System.out.print("+");
+    }
+
+    public static void printWindow() {
+        for(int i = 0; i < SIZE; i++) {
+            System.out.print("|");
+
+            for(int j = 0; j < SIZE; j++)
+                System.out.print(" ");
+
+            System.out.print("|");
+
+            for(int j = 0; j < SIZE; j++)
+                System.out.print(" ");
+
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+}
