8e168b1ca49665857aff98b17e75f3cd418528c1
Yahya <yahyahussein91@gmail.com>
2024-07-09 23:20:56 -0700

    fixed starting index

--- JavaProgammingRefresh/src/PracticeIt/Chapter3/PrintPowersOfTwo.java
+++ JavaProgammingRefresh/src/PracticeIt/Chapter3/PrintPowersOfTwo.java
@@ -3,11 +3,11 @@ package PracticeIt.Chapter3;
 public class PrintPowersOfTwo {
     public static void main(String[] args) {
 
-        printPowersOf2(10);
+        printPowersOf2(3);
     }
     public static void printPowersOf2(int max){
 
-        for (int i = 1; i <= max; i++){
+        for (int i = 0; i <= max; i++){
             System.out.print((int) Math.pow(2, i) + " ");
         }
     }
