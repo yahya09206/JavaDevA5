c56cdc02593b74700e7e49c9a05929f6b17f8a8c
Yahya <yahyahussein91@gmail.com>
2024-07-09 20:57:15 -0700

    print string based on reps using for loops

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter3/PrintStrings.java
@@ -0,0 +1,13 @@
+package PracticeIt.Chapter3;
+
+public class PrintStrings {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printStrings(String name, int reps){
+        for (int i = 0; i <= reps; i++){
+            System.out.print(name);
+        }
+    }
+}
