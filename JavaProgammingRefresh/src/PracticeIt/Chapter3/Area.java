36b0e3d985802eb89d943557019ff653752ca3a1
Yahya <yahyahussein91@gmail.com>
2024-07-16 11:12:23 -0700

    finding area of circle

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter3/Area.java
@@ -0,0 +1,11 @@
+package PracticeIt.Chapter3;
+
+public class Area {
+    public static void main(String[] args) {
+        System.out.println(area(2));
+    }
+    public static double area(double a){
+
+        return Math.PI * (a * a);
+    }
+}
