17d00ecb5bf6935e3574c8003dfee2ba4e543a4a
Yahya <yahyahussein91@gmail.com>
2024-07-17 21:40:07 -0700

    even or odd

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter4/EvenOdd.java
@@ -0,0 +1,17 @@
+package PracticeIt.Chapter4;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a number: ");
+        int number = scanner.nextInt();
+        if (number % 2 == 0){
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+    }
+}
