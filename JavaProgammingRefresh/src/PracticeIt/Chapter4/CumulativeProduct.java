aa2030fb27d3817e13451e0b1137aa05a8b223d9
Yahya <yahyahussein91@gmail.com>
2024-07-19 11:47:10 -0700

    cumalative sum

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter4/CumulativeProduct.java
@@ -0,0 +1,20 @@
+package PracticeIt.Chapter4;
+
+import java.util.Scanner;
+
+public class CumulativeProduct {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many numbers? ");
+        int nums = scanner.nextInt();
+        int product = 1;
+        for (int i = 1; i <= nums; i++){
+            System.out.print("Next number --> ");
+            int num = scanner.nextInt();
+            product *= num;
+        }
+
+        System.out.println("Product = " + product);
+    }
+}
