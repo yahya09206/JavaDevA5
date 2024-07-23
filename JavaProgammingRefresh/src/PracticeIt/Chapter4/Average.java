84f391d21f789e4b6375dd5a3b3b0b554eea3354
Yahya <yahyahussein91@gmail.com>
2024-07-22 22:25:17 -0700

    finding sum

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter4/Average.java
@@ -0,0 +1,12 @@
+package PracticeIt.Chapter4;
+
+public class Average {
+    public static void main(String[] args) {
+
+    }
+    public static double average(int a, int b){
+
+            double sum = a + b;
+            return sum / 2;
+    }
+}
