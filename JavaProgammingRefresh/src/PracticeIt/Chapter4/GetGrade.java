5eced6c9dfff50f7a0820809de86f77120c20143
Yahya <yahyahussein91@gmail.com>
2024-07-22 22:26:11 -0700

    getting grade

---
+++ JavaProgammingRefresh/src/PracticeIt/Chapter4/GetGrade.java
@@ -0,0 +1,25 @@
+package PracticeIt.Chapter4;
+
+public class GetGrade {
+    public static void main(String[] args) {
+
+    }
+    public double getGrade(int score) {
+        if(score < 0 || score > 100)
+            throw new IllegalArgumentException();
+
+        double grade;
+
+        if(score < 60) {
+            grade = 0.0;
+        } else if(60 <= score && score <= 62) {
+            grade = 0.7;
+        } else if(63 <= score && score <= 94) {
+            grade = 0.8 + (score - 63) * 0.1;
+        } else {
+            grade = 4.0;
+        }
+
+        return grade;
+    }
+}
