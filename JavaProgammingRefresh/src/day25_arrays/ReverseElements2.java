ef5c4ba999ecefa76763d90b821a1b782fdc885f
Yahya <yahyahussein91@gmail.com>
2024-08-02 16:55:11 -0700

    reversing elements in array using foreach loop

--- JavaProgammingRefresh/src/day25_arrays/ReverseElements2.java
+++ JavaProgammingRefresh/src/day25_arrays/ReverseElements2.java
@@ -3,8 +3,15 @@ package day25_arrays;
 public class ReverseElements2 {
     public static void main(String[] args) {
 
+        String[] array = {"Java", "Python", "C#"};
 
+        for (String element : array) {
+            String reverse = "";
+            for (int i = element.length() - 1; i >= 0; i--){
+                reverse += element.charAt(i);
+            }
 
-
+            System.out.println(reverse);
+        }
     }
 }
