ac0f48b5aca09d76b94fc31534dbb56f43cef82f
Yahya <yahyahussein91@gmail.com>
2024-08-02 15:25:33 -0700

    finding frequency of elements

--- JavaProgammingRefresh/src/day24_arrays/FrequencyOfElements.java
+++ JavaProgammingRefresh/src/day24_arrays/FrequencyOfElements.java
@@ -3,6 +3,21 @@ package day24_arrays;
 public class FrequencyOfElements {
     public static void main(String[] args) {
 
+        int[] arr = {1, 1, 2, 3, 3, 4};
 
+
+
+        for (int i = 0; i <= arr.length - 1; i++){
+            int element = arr[i];
+            int count = 0;
+            for (int j = 0; j <= arr.length - 1; j++){
+                if (arr[j] == element){
+                    count++;
+                }
+            }
+            if (count == 1){
+                System.out.println(element + " ");
+            }
+        }
     }
 }
