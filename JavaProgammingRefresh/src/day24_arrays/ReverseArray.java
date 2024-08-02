da97b56ad07a48f5266a474c7832a726a73e2f9c
Yahya <yahyahussein91@gmail.com>
2024-08-02 15:37:28 -0700

    reversing elements in array

--- JavaProgammingRefresh/src/day24_arrays/ReverseArray.java
+++ JavaProgammingRefresh/src/day24_arrays/ReverseArray.java
@@ -1,9 +1,18 @@
 package day24_arrays;
 
+import java.util.Arrays;
+
 public class ReverseArray {
     public static void main(String[] args) {
 
+        int[] arr = {1,2,3,4,5};
+        int[] reversedArr = new int[arr.length];
 
+        for (int i = arr.length - 1, j = 0; i >= 0; i--, j++){
+            reversedArr[j] = arr[i];
+        }
+
+        System.out.println(Arrays.toString(reversedArr));
 
     }
 }
