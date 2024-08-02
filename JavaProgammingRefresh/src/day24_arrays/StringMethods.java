54f567c9d7965b5232bea595bec96df4a8d658fe
Yahya <yahyahussein91@gmail.com>
2024-08-02 15:52:34 -0700

    using split method to reverse astring

--- JavaProgammingRefresh/src/day24_arrays/StringMethods.java
+++ JavaProgammingRefresh/src/day24_arrays/StringMethods.java
@@ -3,5 +3,12 @@ package day24_arrays;
 public class StringMethods {
     public static void main(String[] args) {
 
+        // split method
+        String sentence  = "I love java programming language";
+        String[] splitWords = sentence.split(" ");
+
+        for (int i = splitWords.length - 1; i >= 0; i--){
+            System.out.print(splitWords[i] + " ");
+        }
     }
 }
