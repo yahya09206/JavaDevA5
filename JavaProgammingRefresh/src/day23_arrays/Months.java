2c8bf21469e292ef01d6d2b7d1df8b357e610c69
Yahya <yahyahussein91@gmail.com>
2024-08-01 21:56:16 -0700

    print months of the year from user input

--- JavaProgammingRefresh/src/day23_arrays/Months.java
+++ JavaProgammingRefresh/src/day23_arrays/Months.java
@@ -1,7 +1,16 @@
 package day23_arrays;
 
+import java.util.Scanner;
+
 public class Months {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"};
+        System.out.println("Please select a number 1 - 12");
+        int selection = scanner.nextInt();
+
+        System.out.println(months[selection - 1]);
 
 
     }
