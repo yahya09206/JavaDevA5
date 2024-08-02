f3226be0bbf29b8928dd778ac1492f76b0161c75
Yahya <yahyahussein91@gmail.com>
2024-08-02 16:35:32 -0700

    merged two arrays

--- JavaProgammingRefresh/src/day25_arrays/Merge2Arrays.java
+++ JavaProgammingRefresh/src/day25_arrays/Merge2Arrays.java
@@ -1,8 +1,22 @@
 package day25_arrays;
 
+import java.util.Arrays;
+
 public class Merge2Arrays {
     public static void main(String[] args) {
 
+        int[] arr1 = {1, 2, 3, 4},
+                arr2 = {5, 6, 7};
+        int[] arr3 = new int[arr1.length + arr2.length];
 
+        int j = 0;
+        for (int i = 0; i <= arr1.length - 1; i++){
+            arr3[j++] += arr1[i];
+        }
+
+        for (int i = 0; i <= arr2.length - 1; i++){
+            arr3[j++] += arr2[i];
+        }
+        System.out.println(Arrays.toString(arr3));
     }
 }
