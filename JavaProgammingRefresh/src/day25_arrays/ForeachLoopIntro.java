7e8ab1ac1c8437d67508f2c27197bb85a0abe1c7
Yahya <yahyahussein91@gmail.com>
2024-08-02 16:50:06 -0700

    merging two arrays using foreach loop

--- JavaProgammingRefresh/src/day25_arrays/ForeachLoopIntro.java
+++ JavaProgammingRefresh/src/day25_arrays/ForeachLoopIntro.java
@@ -3,5 +3,9 @@ package day25_arrays;
 public class ForeachLoopIntro {
     public static void main(String[] args) {
 
+        String[] names = {"Yeahyea", "Manman"};
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
--- JavaProgammingRefresh/src/day25_arrays/ForeachLoopPractice.java
+++ JavaProgammingRefresh/src/day25_arrays/ForeachLoopPractice.java
@@ -1,7 +1,23 @@
 package day25_arrays;
 
+import java.util.Arrays;
+
 public class ForeachLoopPractice {
     public static void main(String[] args) {
 
+        String[] arr1 = {"A", "B", "C", "D"},
+                arr2 = {"E", "F", "G"};
+        String[] arr3 = new String[arr1.length + arr2.length];
+
+        int j = 0;
+        for (String each : arr1) {
+            arr3[j++] = each;
+        }
+
+        for (String each : arr2) {
+            arr3[j++] = each;
+        }
+
+        System.out.println(Arrays.toString(arr3));
     }
 }
