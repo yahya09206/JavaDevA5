95bcfdc5a4a6efc36f65b0594cab31b37ac3e7ea
Yahya <yahyahussein91@gmail.com>
2024-08-02 15:54:01 -0700

    splitting by specific char

--- JavaProgammingRefresh/src/day24_arrays/StringMethods.java
+++ JavaProgammingRefresh/src/day24_arrays/StringMethods.java
@@ -10,5 +10,11 @@ public class StringMethods {
         for (int i = splitWords.length - 1; i >= 0; i--){
             System.out.print(splitWords[i] + " ");
         }
+
+        System.out.println("--------------------------");
+        String email = "Cydeo@gmail.com";
+        String firstName = email.split("@")[0];
+        System.out.println(firstName);
+
     }
 }
