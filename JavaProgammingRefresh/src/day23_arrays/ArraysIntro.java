350d7af35251c1fc5f751a77f0332ceee7b645a2
Yahya <yahyahussein91@gmail.com>
2024-08-01 21:52:42 -0700

    looping thru list of names

--- JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
+++ JavaProgammingRefresh/src/day23_arrays/ArraysIntro.java
@@ -6,5 +6,13 @@ public class ArraysIntro {
         String[] group2 = {"Feruza", "Kadir", "Ahmet", "Gulistan", "Aykut"};
 
         System.out.println(group2[0]);
+        System.out.println(group2[1]);
+        System.out.println(group2[2]);
+
+        System.out.println("------------------------");
+
+        for (int i = 0; i <= group2.length - 1; i++){
+            System.out.println(group2[i]);
+        }
     }
 }
