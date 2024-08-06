f556310bb49fdeba681f5c4e995d6d59f0ed448e
Yahya <yahyahussein91@gmail.com>
2024-08-06 13:56:29 -0700

    finished with address class

--- JavaProgammingRefresh/src/day40_statics/Address.java
+++ JavaProgammingRefresh/src/day40_statics/Address.java
@@ -5,6 +5,7 @@ public class Address {
     public String buildingNumber, street, city, state;
     public int zipCode;
     public static String country = "USA";
+    public static String planet = "Earth";
 
     public void setInfo(String buildingNumber, String street, String city, String state, int zipCode) {
         this.buildingNumber = buildingNumber;
