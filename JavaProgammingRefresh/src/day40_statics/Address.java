98f764fee8c1ec898597379a632dd24d778ea20f
Yahya <yahyahussein91@gmail.com>
2024-08-06 13:45:56 -0700

    setting up constructor

--- JavaProgammingRefresh/src/day40_statics/Address.java
+++ JavaProgammingRefresh/src/day40_statics/Address.java
@@ -2,6 +2,45 @@ package day40_statics;
 
 public class Address {
 
-    public String buildingNumber, street, city, state, zipCode;
+    public String buildingNumber, street, city, state;
+    public int zipCode;
     public static String country = "USA";
+
+    public Address(String buildingNumber, String street, String city, String state, int zipCode) {
+        this.buildingNumber = buildingNumber;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public void setBuildingNumber(String buildingNumber) {
+        this.buildingNumber = buildingNumber;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public static void setCountry(String country) {
+        Address.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return buildingNumber + street + '\'' +
+                city + state + zipCode;
+    }
 }
