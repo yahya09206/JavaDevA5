f11b1908867bdc7c50909a030984ffd5368f2dc3
Yahya <yahyahussein91@gmail.com>
2024-08-06 13:37:51 -0700

    setting up address class

--- JavaProgammingRefresh/src/day40_statics/Address.java
+++ JavaProgammingRefresh/src/day40_statics/Address.java
@@ -2,25 +2,6 @@ package day40_statics;
 
 public class Address {
 
-    public String buildingNumber;
-    public String streetNumber;
-    public String cityName;
-    public String stateName;
-    public String zipCode;
-    public static String countryName = "USA";
-
-    public Address(String buildingNumber, String streetNumber, String cityName, String stateName, String zipCode) {
-        this.buildingNumber = buildingNumber;
-        this.streetNumber = streetNumber;
-        this.cityName = cityName;
-        this.stateName = stateName;
-        this.zipCode = zipCode;
-    }
-
-    @Override
-    public String toString() {
-        return
-                buildingNumber + " " + streetNumber + "\n" +
-                        cityName + " " + stateName + ", " + zipCode;
-    }
+    public String buildingNumber, street, city, state, zipCode;
+    public static String country = "USA";
 }
--- JavaProgammingRefresh/src/day40_statics/Main.java
+++ JavaProgammingRefresh/src/day40_statics/Main.java
@@ -3,9 +3,6 @@ package day40_statics;
 public class Main {
     public static void main(String[] args) {
 
-        Address address = new Address("10821", "10th Ave", "Seattle", "WA", "98146");
-        System.out.println(address.toString());
 
-        System.out.println("Address.countryName = " + Address.countryName);
     }
 }
