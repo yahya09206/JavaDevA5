9130de26d76acda1d3b922a3ea8439b433feb9d8
Yahya <yahyahussein91@gmail.com>
2024-08-06 13:55:37 -0700

    set address objects and printed

--- JavaProgammingRefresh/src/day40_statics/Address.java
+++ JavaProgammingRefresh/src/day40_statics/Address.java
@@ -6,7 +6,7 @@ public class Address {
     public int zipCode;
     public static String country = "USA";
 
-    public Address(String buildingNumber, String street, String city, String state, int zipCode) {
+    public void setInfo(String buildingNumber, String street, String city, String state, int zipCode) {
         this.buildingNumber = buildingNumber;
         this.street = street;
         this.city = city;
@@ -14,33 +14,11 @@ public class Address {
         this.zipCode = zipCode;
     }
 
-    public void setBuildingNumber(String buildingNumber) {
-        this.buildingNumber = buildingNumber;
-    }
 
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public static void setCountry(String country) {
-        Address.country = country;
-    }
 
     @Override
     public String toString() {
-        return buildingNumber + street + '\'' +
-                city + state + zipCode;
+        return buildingNumber + street + '\n' +
+                city + " " + state + " " + zipCode;
     }
 }
--- JavaProgammingRefresh/src/day40_statics/Main.java
+++ JavaProgammingRefresh/src/day40_statics/Main.java
@@ -1,8 +1,31 @@
 package day40_statics;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Address address = new Address();
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter your building number");
+        String building = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.println("Enter your street name");
+        String street = scanner.nextLine();
+
+        System.out.println("Enter your city name");
+        String city = scanner.nextLine();
+
+        System.out.println("Enter your state name");
+        String state = scanner.nextLine();
+
+        System.out.println("Enter your zipcode");
+        int zipCode = scanner.nextInt();
+
+        address.setInfo(building, street, city, state, zipCode);
+        System.out.println(address);
     }
 }
