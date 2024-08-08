a39c9649d49e6356b0206ae4ca24d7cca6123552
Yahya <yahyahussein91@gmail.com>
2024-08-08 15:17:23 -0700

    access modifiers

--- JavaProgammingRefresh/src/day43_oop_encapsulation/AccessModifiers.java
+++ JavaProgammingRefresh/src/day43_oop_encapsulation/AccessModifiers.java
@@ -2,15 +2,14 @@ package day43_oop_encapsulation;
 
 public class AccessModifiers {
 
-    public static int publicVariable = 100;
-    protected static int protectedVariable = 1000;
-    static int defaultVariable = 200;
-    private static int privateVariable = 200;
+    public static int publicVariable = 100; // visible to every class
+    protected static int protectedVariable = 1000; //
+    private static int privateVariable = 200; // only in class visibility
 
     public static void main(String[] args) {
-        // starting final tomorrow
+
         System.out.println(privateVariable);
-        System.out.println(defaultVariable);
         System.out.println(publicVariable);
     }
+
 }
--- JavaProgammingRefresh/src/day43_oop_encapsulation/Test.java
+++ JavaProgammingRefresh/src/day43_oop_encapsulation/Test.java
@@ -3,9 +3,7 @@ package day43_oop_encapsulation;
 public class Test {
     public static void main(String[] args) {
 
-        System.out.println(AccessModifiers.publicVariable); // reachable within same package
-        System.out.println(AccessModifiers.defaultVariable);// reachable within same package
-        // System.out.println(AccessModifiers.privateVariable); // not reachable within same package
-
+        System.out.println(AccessModifiers.publicVariable);
+        System.out.println(AccessModifiers.protectedVariable);
     }
 }
