220dfc319f23750b0d2b3f1977247ccd49018dc0
Yahya <yahyahussein91@gmail.com>
2024-08-12 16:28:08 -0700

    set up constructor with getters and setters

--- JavaProgammingRefresh/src/day48_recap/cydeoTask/Person.java
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Person.java
@@ -1,12 +1,38 @@
 package day48_recap.cydeoTask;
 
+import java.time.LocalDate;
+
 public class Person {
 
     private String name;
-    private char gender;
-    private int age;
-    private String dateOfBirth;
+    private final char gender;
+    private final int age;
+    private final LocalDate dateOfBirth;
 
+    public Person(char gender, int age, LocalDate dateOfBirth, String name) {
+        this.gender = gender;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+        this.name = name;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
 }
