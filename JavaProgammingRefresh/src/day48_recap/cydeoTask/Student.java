2341a8cab095b709079a371d4233964cb14ab8e5
Yahya <yahyahussein91@gmail.com>
2024-08-12 16:44:40 -0700

    set up student class

--- JavaProgammingRefresh/src/day48_recap/cydeoTask/Student.java
+++ JavaProgammingRefresh/src/day48_recap/cydeoTask/Student.java
@@ -1,5 +1,40 @@
 package day48_recap.cydeoTask;
 
+import java.time.LocalDate;
+
 public class Student extends Person{
 
+    private String schoolName;
+    private int studentID;
+
+    public Student(char gender, LocalDate dateOfBirth, String name, String schoolName, int studentID) {
+        super(gender, dateOfBirth, name);
+        this.schoolName = schoolName;
+        this.studentID = studentID;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                super.toString() +
+                "schoolName='" + schoolName + '\'' +
+                ", studentID=" + studentID +
+                '}';
+    }
 }
