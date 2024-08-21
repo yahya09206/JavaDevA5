f27286edcf1653e86b80a3d4d33becd711e8d671
Yahya <yahyahussein91@gmail.com>
2024-08-21 15:02:59 -0700

    converting different types

--- JavaProgammingRefresh/src/day34_wrapper_class/WrapperClassIntro.java
+++ JavaProgammingRefresh/src/day34_wrapper_class/WrapperClassIntro.java
@@ -6,5 +6,13 @@ public class WrapperClassIntro {
         short s = 10;
         int num = s;
         Integer num2 = (int) s; // Autoboxing: converting primitive value to a wrapper class value
+
+        char ch = '@';
+        Character ch2 = (Character) ch;
+
+        Byte b1 = 10;
+        byte b2 = b1;
+        short b3 = b1;
+        int b4 = b1;
     }
 }
