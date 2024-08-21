c32a0bcdc6f8cad9a4ab0f51e971899150a916ae
Yahya <yahyahussein91@gmail.com>
2024-08-21 15:01:07 -0700

    declaring primmitive and wrapper types

--- JavaProgammingRefresh/src/day34_wrapper_class/WrapperClassIntro.java
+++ JavaProgammingRefresh/src/day34_wrapper_class/WrapperClassIntro.java
@@ -3,6 +3,8 @@ package day34_wrapper_class;
 public class WrapperClassIntro {
     public static void main(String[] args) {
 
-
+        short s = 10;
+        int num = s;
+        Integer num2 = (int) s; // Autoboxing: converting primitive value to a wrapper class value
     }
 }
