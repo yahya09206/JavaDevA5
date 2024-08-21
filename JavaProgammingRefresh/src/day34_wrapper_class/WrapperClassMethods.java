65c80976e4d1f8fa8811dc210125429cb00bea0a
Yahya <yahyahussein91@gmail.com>
2024-08-21 15:17:42 -0700

    done with wrapper classes

--- JavaProgammingRefresh/src/day34_wrapper_class/WrapperClassMethods.java
+++ JavaProgammingRefresh/src/day34_wrapper_class/WrapperClassMethods.java
@@ -5,7 +5,13 @@ public class WrapperClassMethods {
 
         String str = "123";
         int num = Integer.parseInt(str);
+        int num2 = Integer.valueOf(str);
 
         System.out.println(num);
+        System.out.println(num2);
+
+        int num3 = Integer.parseInt(str);
+
+
     }
 }
