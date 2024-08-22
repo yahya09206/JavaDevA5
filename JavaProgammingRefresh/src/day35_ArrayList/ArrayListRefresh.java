7aa02ce1009db485c670905a1f734d4e64e4ca8e
Yahya <yahyahussein91@gmail.com>
2024-08-22 11:20:14 -0700

    constructing array list

--- JavaProgammingRefresh/src/day35_ArrayList/ArrayListRefresh.java
+++ JavaProgammingRefresh/src/day35_ArrayList/ArrayListRefresh.java
@@ -1,7 +1,14 @@
 package day35_ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrayListRefresh {
     public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(4);
+        System.out.println(numbers);
 
 
     }
