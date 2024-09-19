1dd599e9c41f52868e0e6c05baaddd0de68cfe2a
Yahya <yahyahussein91@gmail.com>
2024-09-19 00:24:17 -0700

    fixed for loop

--- JavaProgammingRefresh/src/Day36_ArrayList/Warmup.java
+++ JavaProgammingRefresh/src/Day36_ArrayList/Warmup.java
@@ -19,7 +19,7 @@ public class Warmup {
         list2.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
 
         for (int i = 0; i < list2.size(); i++) {
-            if (list.get(i) % 2 != 0){
+            if (list2.get(i) % 2 != 0){
                 list2.set(i, list2.get(i) * 2);
             }
         }
