5f43a6b68ef52f1185f27653c6bb68671b31dc93
Yahya <yahyahussein91@gmail.com>
2024-09-19 00:15:56 -0700

    program for adding a zero to last index

--- JavaProgammingRefresh/src/Day36_ArrayList/Warmup.java
+++ JavaProgammingRefresh/src/Day36_ArrayList/Warmup.java
@@ -1,7 +1,14 @@
 package Day36_ArrayList;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Warmup {
     public static void main(String[] args) {
+
+        //Set last element of arrayList to 0
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5));
 
     }
 }
