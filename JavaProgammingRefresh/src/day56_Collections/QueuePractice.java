b1b284c6d8a985e1f17225c1ae96f05c5d67199a
Yahya <yahyahussein91@gmail.com>
2024-10-12 10:31:13 -0700

    different types of queues

---
+++ JavaProgammingRefresh/src/day56_Collections/QueuePractice.java
@@ -0,0 +1,21 @@
+package day56_Collections;
+
+import java.util.*;
+
+public class QueuePractice {
+    public static void main(String[] args) {
+
+        Queue<Integer> q1 = new PriorityQueue<>();
+        q1.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        Queue<Integer> q2 = new ArrayDeque<>();
+        q2.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        Queue<Integer> q3 = new LinkedList<>();
+        q3.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        System.out.println("q1 = " + q1);
+        System.out.println("q2 = " + q2);
+        System.out.println("q3 = " + q3);
+    }
+}
--- JavaProgammingRefresh/src/day56_Collections/SetInterface.java
+++ JavaProgammingRefresh/src/day56_Collections/SetInterface.java
@@ -46,7 +46,5 @@ public class SetInterface {
         }
 
         System.out.println("result = " + result);
-
-
     }
 }
