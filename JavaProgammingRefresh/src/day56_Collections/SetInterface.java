e11afedeff5d9d1a9393763ad21ec2c13c5c2059
Yahya <yahyahussein91@gmail.com>
2024-10-12 02:15:28 -0700

    checking insertion orders of different objects

--- JavaProgammingRefresh/src/day56_Collections/SetInterface.java
+++ JavaProgammingRefresh/src/day56_Collections/SetInterface.java
@@ -1,7 +1,25 @@
 package day56_Collections;
 
+import java.util.*;
+
 public class SetInterface {
     public static void main(String[] args) {
+
+        String[] arr = {"Wooden Spoon", "Book", "Pen", "Wooden Spoon", "Wooden Spoon", "Wooden Spoon", "Wooden Spoon"};
+
+        // No order
+        Set<String> items = new HashSet<>(Arrays.asList(arr));
+        items.add(null);
+        // Insertion order
+        Set<String> items2 = new LinkedHashSet<>(Arrays.asList(arr));
+        items2.add(null);
+        // Sorted order Ascending, does not accept null
+        Set<String> items3 = new TreeSet<>(Arrays.asList(arr));
+        // items3.add(null);
+
+        System.out.println("items = " + items);
+        System.out.println("items2 = " + items2);
+        System.out.println("items3 = " + items3);
 
     }
 }
