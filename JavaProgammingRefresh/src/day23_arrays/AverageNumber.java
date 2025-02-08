514cb8dce9e6254c2e590695be847d01d2a2be68
Yahya <yahyahussein91@gmail.com>
2025-02-08 01:48:01 -0800

    prompting user for amount of numbers

--- JavaProgammingRefresh/src/day23_arrays/AverageNumber.java
+++ JavaProgammingRefresh/src/day23_arrays/AverageNumber.java
@@ -1,8 +1,17 @@
 package day23_arrays;
 
+import java.util.Scanner;
+
 public class AverageNumber {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many numbers do you want to enter? ");
+        int[] nums = new int[scanner.nextInt()];
+
+        for (int i = 0; i <= nums.length - 1; i++){
+            nums[i] = scanner.nextInt();
+        }
 
     }
 }
