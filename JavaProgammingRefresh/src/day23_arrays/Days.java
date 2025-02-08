53604cd94097bd56fcc3707b69fba85002566167
Yahya <yahyahussein91@gmail.com>
2025-02-08 00:59:15 -0800

    prompting user for number and printing month

--- JavaProgammingRefresh/src/day23_arrays/Days.java
+++ JavaProgammingRefresh/src/day23_arrays/Days.java
@@ -3,7 +3,5 @@ package day23_arrays;
 public class Days {
     public static void main(String[] args) {
 
-
-
     }
 }
--- JavaProgammingRefresh/src/day23_arrays/Months.java
+++ JavaProgammingRefresh/src/day23_arrays/Months.java
@@ -1,8 +1,16 @@
 package day23_arrays;
 
+import java.util.Scanner;
+
 public class Months {
     public static void main(String[] args) {
 
+        String[] months = {"Jan", "Feb", "March", "April", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"};
 
+        System.out.println("Enter a number: ");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        System.out.println(months[n-1]);
     }
 }
