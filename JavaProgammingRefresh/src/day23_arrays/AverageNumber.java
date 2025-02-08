07aaff0946aebb6aba6642d2acffa7f0517a8ba5
Yahya <yahyahussein91@gmail.com>
2025-02-08 01:50:02 -0800

    printing out average of all the numbers

--- JavaProgammingRefresh/src/day23_arrays/AverageNumber.java
+++ JavaProgammingRefresh/src/day23_arrays/AverageNumber.java
@@ -8,10 +8,15 @@ public class AverageNumber {
         Scanner scanner = new Scanner(System.in);
         System.out.println("How many numbers do you want to enter? ");
         int[] nums = new int[scanner.nextInt()];
+        int sum = 0;
 
         for (int i = 0; i <= nums.length - 1; i++){
             nums[i] = scanner.nextInt();
+            sum += nums[i];
         }
 
+        System.out.println("sum = " + sum);
+        double average = (double) sum / nums.length;
+        System.out.println("average = " + average);
     }
 }
