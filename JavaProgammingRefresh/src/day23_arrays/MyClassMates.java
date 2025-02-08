aa9d6c622fb926fc72261eb332c3ab0c760ad541
Yahya <yahyahussein91@gmail.com>
2025-02-08 01:09:03 -0800

    toString method to print elements from array

--- JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
+++ JavaProgammingRefresh/src/day23_arrays/MyClassMates.java
@@ -1,7 +1,17 @@
 package day23_arrays;
 
+import java.util.Arrays;
+
 public class MyClassMates {
     public static void main(String[] args) {
+
+
+        String[] classMates = new String[3];
+        classMates[0] = "Michelle";
+        classMates[1] = "Obama";
+        classMates[2] = "Joe";
+
+        System.out.println(Arrays.toString(classMates));
 
 
     }
