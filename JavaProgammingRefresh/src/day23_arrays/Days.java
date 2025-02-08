2c4eb02195a72c6c961aa8d160f9093b62073198
Yahya <yahyahussein91@gmail.com>
2025-02-08 01:07:03 -0800

    classmates

--- JavaProgammingRefresh/src/day23_arrays/Days.java
+++ JavaProgammingRefresh/src/day23_arrays/Days.java
@@ -1,8 +1,15 @@
 package day23_arrays;
 
+import java.util.Scanner;
+
 public class Days {
     public static void main(String[] args) {
 
+        String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"}; // size of 7
 
+        System.out.println("Enter a number: ");
+        int n = new Scanner(System.in).nextInt();
+
+        System.out.println(days[n-1]);
     }
 }
