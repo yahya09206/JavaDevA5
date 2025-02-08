f3fbfed082db20f7461f78db3b57731a8c35b9e4
Yahya <yahyahussein91@gmail.com>
2025-02-08 01:39:55 -0800

    getting name of person with highest score

--- JavaProgammingRefresh/src/day23_arrays/HighestAndLowestScores.java
+++ JavaProgammingRefresh/src/day23_arrays/HighestAndLowestScores.java
@@ -3,6 +3,21 @@ package day23_arrays;
 public class HighestAndLowestScores {
     public static void main(String[] args) {
 
+        int[] scores = {85, 70, 95, 90, 100};
+        String[] names = {"Mike", "Adam", "Tony", "John", "Amy"};
+        int max = scores[0];
+        String nameOfHighestScore = "";
 
+        for (int i = 0; i <= scores.length - 1; i++){
+            String eachName = names[i];
+            int eachScore = scores[i];
+            if (eachScore > max){
+                max = eachScore;
+                nameOfHighestScore = eachName;
+            }
+        }
+
+        System.out.println("max = " + max);
+        System.out.println("nameOfHighestScore = " + nameOfHighestScore);
     }
 }
