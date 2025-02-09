7214e5333235839b75b11005303251274bc037c1
Yahya <yahyahussein91@gmail.com>
2025-02-09 02:54:46 -0800

    reversing using split method

--- JavaProgammingRefresh/src/day24_arrays/StringMethods.java
+++ JavaProgammingRefresh/src/day24_arrays/StringMethods.java
@@ -1,8 +1,19 @@
 package day24_arrays;
 
+import java.util.Arrays;
+
 public class StringMethods {
     public static void main(String[] args) {
 
+        String str = "I love learning java programming language";
+        String[] words = str.split(" ");
 
+        System.out.println("Arrays.toString(str) = " + Arrays.toString(words));
+        System.out.println("words.length = " + words.length);
+
+
+        for (int i = words.length - 1; i >= 0; i--){
+            System.out.print(words[i] + " ");
+        }
     }
 }
