f6df5bb25e75d5909a1feab4ef28b55d0f0939ad
Yahya <yahyahussein91@gmail.com>
2025-02-09 02:47:54 -0800

    added clarifying comments

--- JavaProgammingRefresh/src/day24_arrays/ReverseArray.java
+++ JavaProgammingRefresh/src/day24_arrays/ReverseArray.java
@@ -6,8 +6,10 @@ public class ReverseArray {
     public static void main(String[] args) {
 
         int[] nums = {1, 2, 3, 4, 5};
+        // store result in new array
         int[] reversed = new int[nums.length];
 
+        // variable to track original array and variable to track index of new array
         for (int i = nums.length - 1, j = 0; i >= 0; i--, j++){
             reversed[j] += nums[i];
         }
