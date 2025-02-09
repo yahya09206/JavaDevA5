d8a825d74570cb81e2f3332b51bc1d473beaa780
Yahya <yahyahussein91@gmail.com>
2025-02-09 02:59:52 -0800

    checking if anagram

--- JavaProgammingRefresh/src/day24_arrays/Anagram.java
+++ JavaProgammingRefresh/src/day24_arrays/Anagram.java
@@ -1,9 +1,24 @@
 package day24_arrays;
 
+import java.util.Arrays;
+
 // will be reviewing
 public class Anagram {
     public static void main(String[] args) {
 
+        String str1 = "heart";
+        String str2 = "earth";
 
+        // use toCharArray to split by characters
+        char[] char1 = str1.toCharArray();
+        char[] char2 = str2.toCharArray();
+
+        // sort characters
+        Arrays.sort(char1);
+        Arrays.sort(char2);
+
+        // boolean to compare values using Arrays.equals
+        boolean isAnagram = Arrays.equals(char1, char2);
+        System.out.println("isAnagram = " + isAnagram);
     }
 }
--- JavaProgammingRefresh/src/day24_arrays/StringMethods.java
+++ JavaProgammingRefresh/src/day24_arrays/StringMethods.java
@@ -6,13 +6,16 @@ public class StringMethods {
     public static void main(String[] args) {
 
         String str = "I love learning java programming language";
+        // split it up into an array
         String[] words = str.split(" ");
 
         System.out.println("Arrays.toString(str) = " + Arrays.toString(words));
         System.out.println("words.length = " + words.length);
 
 
+        // loop through split up array
         for (int i = words.length - 1; i >= 0; i--){
+            // print each word with space in between
             System.out.print(words[i] + " ");
         }
     }
