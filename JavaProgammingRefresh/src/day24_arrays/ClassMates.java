65e96a683ef7b79ffb2e1ca4d43262abf6c725a1
Yahya <yahyahussein91@gmail.com>
2025-02-09 01:30:48 -0800

    getting first three chars of name

--- JavaProgammingRefresh/src/day24_arrays/ClassMates.java
+++ JavaProgammingRefresh/src/day24_arrays/ClassMates.java
@@ -3,6 +3,11 @@ package day24_arrays;
 public class ClassMates {
     public static void main(String[] args) {
 
+        String[] myClassMates = {"Johnny", "Mikey", "Sharon", "Nikky"};
+        for(int i = 0; i <= myClassMates.length - 1; i++){
+            String firstThree = myClassMates[i].substring(0,3);
+            System.out.println(firstThree);
+        }
 
     }
 }
