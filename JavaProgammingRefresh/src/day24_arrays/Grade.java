ac6a812a5d8697239f052b86360d116c78d20b68
Yahya <yahyahussein91@gmail.com>
2025-02-09 02:03:11 -0800

    matching grades with student

--- JavaProgammingRefresh/src/day24_arrays/Grade.java
+++ JavaProgammingRefresh/src/day24_arrays/Grade.java
@@ -3,6 +3,12 @@ package day24_arrays;
 public class Grade {
     public static void main(String[] args) {
 
+        String[] names = {"Anna", "Nancy", "Sarah"};
+        int[] scores = {90, 75, 80};
+        char[] grades = new char[names.length];
 
+        if (scores[0] >= 90){
+            grades[0] = 'A';
+        }
     }
 }
