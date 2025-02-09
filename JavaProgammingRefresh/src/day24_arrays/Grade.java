dd690636f16cfbbf1078478e19719c4639bf956a
Yahya <yahyahussein91@gmail.com>
2025-02-09 02:21:32 -0800

    added more names to reach rest of code

--- JavaProgammingRefresh/src/day24_arrays/Grade.java
+++ JavaProgammingRefresh/src/day24_arrays/Grade.java
@@ -3,25 +3,25 @@ package day24_arrays;
 public class Grade {
     public static void main(String[] args) {
 
-        String[] names = {"Anna", "Nancy", "Sarah"};
-        int[] scores = {90, 75, 80};
+        String[] names = {"Anna", "Nancy", "Sarah", "Jenny", "Benny"};
+        int[] scores = {90, 75, 80, 65, 52};
         char[] grades = new char[names.length];
 
         for (int i = 0; i <= grades.length - 1; i++){
 
-            int score = scores[i];
-
-            if (score >= 90){
+            if (scores[i] >= 90){
                 grades[i] = 'A';
-            } else if (score >= 80){
+            } else if (scores[i] >= 80){
                 grades[i] = 'B';
-            } else if (score >= 70){
+            } else if (scores[i] >= 70){
                 grades[i] = 'C';
-            } else if (score >= 60){
+            } else if (scores[i] >= 60){
                 grades[i] = 'D';
             } else {
                 grades[i] = 'F';
             }
+
+            System.out.println(names[i] + ": score is " + scores[i] + ", made " + grades[i]);
         }
     }
 }
